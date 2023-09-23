88b2acaa423ba6e039f8b84e1986a7f76c067641
Ret Server <retserver@gmail.com>
2023-09-23 21:03:10 +0000

    Fix Amondson plat trade

--- bazaar/Galic_Amondson.lua
+++ bazaar/Galic_Amondson.lua
@@ -1,26 +1,27 @@
 -- items: 21805, 21820, 21806, 21821, 21807, 21822, 21808, 21823
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("Greetings " .. e.other:GetName() .. ". If you're looking to sell a white horse, I'll be happy to give you a fair price.  If you've got a different color horse you'd like to sell talk to one of my brothers.");
+		e.self:Say("Greetings " .. e.other:GetName() .. ". Were you hoping I would have a quest for you?... BEGONE!");
+		$client->MovePC($client->GetBindZoneID, $client->GetBindX, $client->GetBindY, $client->GetBindZ, $client->GetBindHeading);
 	end
 end
 
-function event_trade(e)
-	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 21805})) then
-		e.other:SummonItem(21820); -- Item: Bag of Platinum Pieces
+--function event_trade(e)
+	--local item_lib = require("items");
+	--if(item_lib.check_turn_in(e.trade, {item1 = 21805})) then
+	--	e.other:SummonItem(21820); -- Item: Bag of Platinum Pieces
 
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 21806})) then
-		e.other:SummonItem(21821); -- Item: Heavy Bag of Platinum
+	--elseif(item_lib.check_turn_in(e.trade, {item1 = 21806})) then
+	--	e.other:SummonItem(21821); -- Item: Heavy Bag of Platinum
 
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 21807})) then
-		e.other:SummonItem(21822); -- Item: Big Bag of Platinum
+	--elseif(item_lib.check_turn_in(e.trade, {item1 = 21807})) then
+	--	e.other:SummonItem(21822); -- Item: Big Bag of Platinum
 
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 21808})) then
-		e.other:SummonItem(21823); -- Item: Huge Bag of Platinum
-	end
+	--elseif(item_lib.check_turn_in(e.trade, {item1 = 21808})) then
+	--	e.other:SummonItem(21823); -- Item: Huge Bag of Platinum
+	--end
 	--item_lib.return_items(e.self, e.other, e.trade)
-end
+--end
 
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd
--- bazaar/Wendal_Amondson.lua
+++ bazaar/Wendal_Amondson.lua
@@ -1,26 +1,27 @@
 -- items: 21810, 21820, 21811, 21821, 21812, 21822, 21813, 21823
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("Greetings " .. e.other:GetName() .. ". If you're looking to sell a black horse, I'll be happy to give you a fair price.  If you've got a different color horse you'd like to sell talk to one of my brothers.");
+		e.self:Say("Greetings " .. e.other:GetName() .. ". Were you hoping I would have a quest for you?... BEGONE!");
+		$client->MovePC($client->GetBindZoneID, $client->GetBindX, $client->GetBindY, $client->GetBindZ, $client->GetBindHeading);
 	end
 end
 
-function event_trade(e)
-	local item_lib = require("items");
-	if(item_lib.check_turn_in(e.trade, {item1 = 21810})) then
-		e.other:SummonItem(21820); -- Item: Bag of Platinum Pieces
+-- function event_trade(e)
+	-- local item_lib = require("items");
+	-- if(item_lib.check_turn_in(e.trade, {item1 = 21810})) then
+	--	e.other:SummonItem(21820); -- Item: Bag of Platinum Pieces
 
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 21811})) then
-		e.other:SummonItem(21821); -- Item: Heavy Bag of Platinum
+	-- elseif(item_lib.check_turn_in(e.trade, {item1 = 21811})) then
+		--e.other:SummonItem(21821); -- Item: Heavy Bag of Platinum
 
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 21812})) then
-		e.other:SummonItem(21822); -- Item: Big Bag of Platinum
+	--elseif(item_lib.check_turn_in(e.trade, {item1 = 21812})) then
+		--e.other:SummonItem(21822); -- Item: Big Bag of Platinum
 
-	elseif(item_lib.check_turn_in(e.trade, {item1 = 21813})) then
-		e.other:SummonItem(21823); -- Item: Huge Bag of Platinum
-	end
+	--elseif(item_lib.check_turn_in(e.trade, {item1 = 21813})) then
+		--e.other:SummonItem(21823); -- Item: Huge Bag of Platinum
+	--end
 	--item_lib.return_items(e.self, e.other, e.trade)
-end
+-- end
 
 -------------------------------------------------------------------------------------------------
 -- Converted to .lua using MATLAB converter written by Stryd
