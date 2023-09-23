e7713a526a1f33c46a56ff20212b30363be187a4
Ret Server <retserver@gmail.com>
2023-09-23 21:07:55 +0000

    Forced normal item reward for epic quest

--- lakerathe/Misty_Tekcihta.pl
+++ lakerathe/Misty_Tekcihta.pl
@@ -17,7 +17,7 @@ sub EVENT_SAY {
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 20534 => 1)) {
     quest::emote("hands you a small ring.");
-    quest::summonitem(20379); # Item: Proof of Speed
+    quest::summonfixeditem(20379); # Item: Proof of Speed
   }
   plugin::return_items(\%itemcount);
 }
