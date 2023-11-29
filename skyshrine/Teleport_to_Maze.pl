3170083673cd3bd4cc65b1e91f8515678a3ca0e4
RetributionEQ <120991589+RetributionEQ@users.noreply.github.com>
2023-11-29 16:49:50 +0000

    Maze teleporter brings you to the END of maze

--- skyshrine/Teleport_to_Maze.pl
+++ skyshrine/Teleport_to_Maze.pl
@@ -4,9 +4,9 @@ $instanceid = quest::GetInstanceID();
 
 sub EVENT_SAY {
    if($text=~/hail/i)   {
-		quest::say("I can grant you passage to the [maze]! This is helpful for Skyshrine class armor quests. Only use this in the Open World, not DZ's!");
+		quest::say("I can grant you passage beyond the [maze]! This is helpful for Skyshrine class armor quests and for people that don't like being stuck in infinite loops. Please just skip the maze unless you are in the open world.");
    }
    elsif($text=~/maze/i)	{
-		quest::MovePCInstance(114, $instanceid, -239, 743, 258, 127);
+		quest::MovePCInstance(114, $instanceid, 659, -60, 3, 385);
    }
 }
