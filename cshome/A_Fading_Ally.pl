a8db5695a1736ec83497cfd5edb8d9f0ec20fe93
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-12-13 21:44:08 -0600

    derp

--- cshome/A_Fading_Ally.pl
+++ cshome/A_Fading_Ally.pl
@@ -2,7 +2,7 @@ sub EVENT_SAY {
 	if ($client->GetGM()) {
 		if ($text=~/god on/i) {
 			quest::set_data("god-open", 10);
-		} elsif ($test=~/god off/i) {
+		} elsif ($text=~/god off/i) {
 			quest::delete_data("god-open");
 		}
 	}
