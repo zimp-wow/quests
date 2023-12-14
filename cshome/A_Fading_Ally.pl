6703e4cfd5e5858a7111b0a192489ca77491b493
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-12-13 21:44:58 -0600

    add some ffeedback

--- cshome/A_Fading_Ally.pl
+++ cshome/A_Fading_Ally.pl
@@ -2,8 +2,10 @@ sub EVENT_SAY {
 	if ($client->GetGM()) {
 		if ($text=~/god on/i) {
 			quest::set_data("god-open", 10);
+			quest::say("Enabling Gates of Discord Access");
 		} elsif ($text=~/god off/i) {
 			quest::delete_data("god-open");
+			quest::say("Disabling Gates of Discord Access");
 		}
 	}
 }
