99f3f8fa966c2e211d52f8c79524925af3aae210
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-01 18:45:45 -0600

    f

--- abysmal/Magus_Pellen.lua
+++ abysmal/Magus_Pellen.lua
@@ -5,8 +5,13 @@ function event_say(e)
 		e.self:Say("I can provide you with travel to [" .. eq.say_link("Natimbi",false,"Natimbi") .. "] and [" .. eq.say_link("Nedaria",false,"Nedaria") .. "]'s Landing with our Farstone magic.  Just tell me where you'd like to go and I shall send you.");
 	elseif(e.message:findi("natimbi") and eq.get_data("god-open") and (eq.get_data(e.other:AccountID() .. "-saryrn-flag") > 0 or eq.get_data(e.other:AccountID() .. "-kunark-flag") >= 20)) then
 		e.other:MovePC(280, -1557, -853, 241); -- Zone: natimbi
-	elseif e.message:findi("nedaria") then
-		e.self:CastSpell(4580,e.other:GetID(),0,1); -- Spell: Teleport: Nedaria
+	elseif (e.message:findi("natimbi") and eq.get_data("god-open")) then
+        local saryrn_flag = eq.get_data(e.other:AccountID() .. "-saryrn-flag")
+        local kunark_flag = eq.get_data(e.other:AccountID() .. "-kunark-flag")
+    
+        if (tonumber(saryrn_flag or "0") > 0 or tonumber(kunark_flag or "0") >= 20) then
+            e.other:MovePC(280, -1557, -853, 241, 180); -- Zone: natimbi
+        end
 	end
 end
 
