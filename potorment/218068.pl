598043388547572cc796d2802f5538c0d164e9dc
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-01 18:15:50 -0600

    f

--- potorment/218068.pl
+++ potorment/218068.pl
@@ -9,7 +9,7 @@ sub EVENT_SAY {
 		quest::setglobal("pop_pot_saryrn", 1, 5, "F");
 		$client->Message(12,"The Planar Projection seems to flicker in and out of existence. It seems to be impressed and grateful for the death of Saryrn.");
 		$client->Message(4,"You receive a character flag!");
-		quest::set_data($client->AccountID() . "-saryrn-flag");
+		quest::set_data($client->AccountID() . "-saryrn-flag", 1);
 		$client->Message(4, "You have gained a progression flag!");
 	}
 
