e25c314dd628dccbc9c88e1708f701e8dd4f3b9f
RetributionEQ <info@retributioneq.com>
2024-01-06 04:14:53 +0000

    Tublik now returns Icon AND gives say link to check prog at this step

--- kodtaz/Tublik_Narwethar.pl
+++ kodtaz/Tublik_Narwethar.pl
@@ -298,12 +298,13 @@ sub EVENT_ITEM {
       $client->Message(4, "Finished! - You've constructed your Icon of the Altar!");
       quest::setglobal("ikky",14,5,"F");
 	}
-	quest::summonitem(60173); # Item: Icon of the Altar
+	quest::summonfixeditem(60173); # Item: Icon of the Altar
   }
   if (defined($qglobals{ikky}) && ($qglobals{ikky} < 14)) {
     if (plugin::check_handin(\%itemcount, 60173 => 1)) {
-      quest::say("You can't have this? What kind of trickery is this? I'm alerting the proper autorities you thief.");
+      quest::say("You can't have this? What kind of trickery is this? You haven't completed the [" . quest::saylink("final step") . "] yet have you?.");
     }
+  quest::summonfixeditem(60173); # Item: Icon of the Altar
   }
   if (defined($qglobals{ikky}) && ($qglobals{ikky} >= 10)) {
     if (plugin::check_handin(\%itemcount, 60160 => 1)) {
