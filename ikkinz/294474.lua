155a5a9456d42b8acfcf76ad47a59ace840972de
RetributionEQ <info@retributioneq.com>
2024-01-06 00:34:49 +0000

    Remove immunity for real.

--- ikkinz/294474.lua
+++ ikkinz/294474.lua
@@ -3,7 +3,10 @@
 --(center)(regen)
 function event_signal(e)
   if (e.signal == 1) then
-    e.self:SetSpecialAbility(35, 0); -- turn off immunity
+    e.self:SetSpecialAbility(24, 0); --turn off immunity
+		e.self:SetSpecialAbility(35, 0); -- turn off immunity
+		e.self:SetSpecialAbility(19, 0);
+		e.self:SetSpecialAbility(20, 0);
   end
 end
 
