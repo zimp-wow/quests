2ea4eb3a08b6176e4c6aaf51875f70c01e5ff035
RetributionEQ <info@retributioneq.com>
2024-01-07 06:07:46 +0000

    Adding removal of melee/magic immune

--- inktuta/a_clay_monolith___.lua
+++ inktuta/a_clay_monolith___.lua
@@ -1,6 +1,8 @@
 -- a_clay_monolith #296051 - Fourth set at bottom of stairs
 
 function event_signal(e)
-	e.self:SetSpecialAbility(24, 0);
-	e.self:SetSpecialAbility(35, 0);
+	e.self:SetSpecialAbility(35, 0); --turn off immunity
+	e.self:SetSpecialAbility(19, 0); --turn off melee immune
+	e.self:SetSpecialAbility(20, 0); --turn off magic immune
+	e.self:SetSpecialAbility(24, 0); --turn off anti aggro
 end
