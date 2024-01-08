5f77b0505b6b92dfe4032edfb4c18b9ed7240619
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-07 19:56:37 -0600

    set gas chamber player minimum to 1

--- uqua/player.lua
+++ uqua/player.lua
@@ -1,6 +1,6 @@
 local gas_1_range = {-293, -263, -769, -718}
 local gas_2_range = {-886, -850, -801, -750}
-local player_minimum = 3;
+local player_minimum = 1;
 
 function event_enter_zone(e)
 	local qglobals = eq.get_qglobals(e.self); 
