9d752ed5a58918defa1e23443a2d802196edaf2b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-14 13:43:31 -0600

    brute forec fix to frog zone loops

--- crescent/player.pl
+++ crescent/player.pl
@@ -1,4 +1,9 @@
-sub EVENT_CONNECT {
+sub EVENT_CONNECT { 
+    if ($client->GetRace() == 330) {
+      if ($client->GetDeity() = 208) {
+        $client->MovePC(8, 120, 129, -24, 0);
+      }
+    }
 
     $client->SendToGuildHall();
   }
