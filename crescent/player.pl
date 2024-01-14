5b76377fa3489950e092ec6a505a6126526a5c82
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-14 13:44:37 -0600

    be smarter

--- crescent/player.pl
+++ crescent/player.pl
@@ -1,17 +1,22 @@
 sub EVENT_CONNECT { 
+    check_frog_zone();
+    $client->SendToGuildHall();
+  }
+
+  sub EVENT_ZONE {
+    check_frog_zone();
+    $client->SendToGuildHall();
+  }
+
+  sub EVENT_ENTERZONE {
+    check_frog_zone();
+    $client->SendToGuildHall();
+  }
+
+  sub check_frog_zone {
     if ($client->GetRace() == 330) {
       if ($client->GetDeity() = 208) {
         $client->MovePC(8, 120, 129, -24, 0);
       }
     }
-
-    $client->SendToGuildHall();
-  }
-
-  sub EVENT_ZONE {
-    $client->SendToGuildHall();
-  }
-
-  sub EVENT_ENTERZONE {
-    $client->SendToGuildHall();
   }
