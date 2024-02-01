ca37d16f7060b8a52f201c989ff78eaf5772df63
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-01 09:59:19 -0600

    wat

--- bazaar/Soulbinder_Novalu.pl
+++ bazaar/Soulbinder_Novalu.pl
@@ -19,9 +19,3 @@ sub EVENT_ENTER
 {
 	quest::signal(202273,5); #Qadar
 }
-	if($text=~/hail/i){
-		plugin::Whisper("Greetings ${name} . When a hero of our world is slain their soul returns to the place it was last bound and the body is reincarnated. As a member of the Order of Eternity  it is my duty to [$bindme] to this location if that is your wish.");
-	} elsif($text=~/bind your soul/i) {
-	    plugin::Whisper("Binding your soul. You will return here when you die.");
-	    quest::selfcast(2049);
-	}
