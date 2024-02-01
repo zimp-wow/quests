c650f7d349445f01ac48ca8bb90137e1a932292d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-31 23:52:29 -0600

    testing adding to existing plugin instead

--- plugins/anim.pl
+++ plugins/anim.pl
@@ -2,6 +2,58 @@
 #::: Description: Will take in an easy to remember name or string and take care of the conversion to ID in the scripting process
 #::: Author: Akkadius
 
+# Check if a particular piece of data (by zone description) is present
+sub has_zone_entry {
+    my ($accountID, $zone_desc, $suffix) = @_;
+    my $teleport_zones = get_zone_data_for_account($accountID, $suffix);
+
+    return exists($teleport_zones->{$zone_desc});
+}
+
+# Get character's saved zone data
+sub get_zone_data_for_account {
+    my ($accountID, $suffix) = @_;
+    my $charKey = $accountID . "-TL-Account-" . $suffix;
+
+    my $charDataString = quest::get_data($charKey);
+
+    my %teleport_zones;
+    my @zone_entries = split /:/, $charDataString;
+
+    foreach my $entry (@zone_entries) {
+        my @tokens = split /,/, $entry;
+        $teleport_zones{$tokens[0]} = [@tokens[1..$#tokens]];
+    }
+
+    return \%teleport_zones;
+}
+
+# Add (or overwrite) data to teleport_zones
+# Usage:
+#    add_zone_entry(12345, "Zone4", ['data7', 'data8'], '-K');
+sub add_zone_entry {
+    my ($accountID, $zone_name, $zone_data, $suffix) = @_;
+    my $teleport_zones = get_zone_data_for_account($accountID, $suffix);
+    $teleport_zones->{$zone_name} = $zone_data;
+    set_zone_data_for_account($accountID, $teleport_zones, $suffix);
+}
+
+sub set_zone_data_for_account {
+    my ($accountID, $zone_data_hash_ref, $suffix) = @_;
+    my $charKey = $accountID . "-TL-Account-" . $suffix;
+
+    my @data_entries;
+
+    while (my ($desc, $zone_data) = each %{$zone_data_hash_ref}) {
+        my $entry = join(",", $desc, @{$zone_data});
+        push @data_entries, $entry;
+    }
+
+    my $charDataString = join(":", @data_entries);
+
+    quest::set_data($charKey, $charDataString);
+}
+
 sub DoAnim {
 	my $client = plugin::val('$client');
 	my $text = $_[0];
--- plugins/catawputils.pl
+++
@@ -1,7 +0,0 @@
-# Check if a particular piece of data (by zone description) is present
-sub has_zone_entry {
-    my ($accountID, $zone_desc, $suffix) = @_;
-    #my $teleport_zones = get_zone_data_for_account($accountID, $suffix);
-
-    return exists($teleport_zones->{$zone_desc});
-}
