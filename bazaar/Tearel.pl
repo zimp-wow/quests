953e1c360e8fffcc25c354bc67b2b760ca7b4a8f
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-01-31 23:33:05 -0600

    Lots of hacking
    rename to try to figure out why this doesn't load
    
    minor error
    
    wtf
    
    I'm at a loss here why my plugin isn't loading..
    
    test
    
    test
    
    test
    
    test
    
    wtf
    
    minimum method?
    
    testing adding to existing plugin instead
    
    test
    
    starting work on tearel
    
    progress
    
    test
    
    test
    
    oops
    
    oops
    
    test
    
    I think I must be tired.
    
    test
    
    test
    
    refinements
    
    oops
    
    f
    
    test
    
    f

--- bazaar/Tearel.pl
+++ bazaar/Tearel.pl
@@ -1,6 +1,45 @@
 sub EVENT_SAY {
+
+  my $continent_regex = join('|', map { my $continent = plugin::get_continent_by_suffix($_); $continent =~ s/\s+//g; quotemeta($continent) } plugin::get_suffixes());
+  my $zone_data       = plugin::get_zone_data($client->AccountID());
+
+  quest::debug($continent_regex);
+
   if ($text=~/hail/i) {
-    quest::say("$myvalue Hello $name! Would you like to set your guild teleportation circle? The process is simple! Purchase the teleportation stone of your choosing then give it to me. I will then enchant the map to take you to your destination! Simply click it and you will be gone!");
+    quest::say("Hello $name! Would you like to set your guild teleportation circle? The process is simple! 
+                Purchase the teleportation stone of your choosing then give it to me. I will then enchant 
+                the map to take you to your destination! Simply click it and you will be gone!");
+  }
+
+  if ($client->GetGM()) {
+
+  if ($text=~/hail/i) { 
+    quest::whisper("Hello $name! I here to transport you to whichever destination you require! I can sell you
+                    [teleportion stones] which can attune this magic map beside me to several notable locations 
+                    throughout Norrath. If you are a more experienced adventurer, I can [transport] you to 
+                    places that you have strong memories of.");
+  }
+
+  if ($text=~/teleportion stones/i) {
+    quest::whisper("Absolutely. The process is simple! Purchase the teleportation stone of your choosing 
+                    then give it to me. I will then enchant the map to take you to your destination! 
+                    Simply click it and you will be gone!");
+  }
+  if ($text=~/transport/i) {
+    quest::whisper("Of course. Tell me about the place that you remember.");
+    $client->Message(257, " ------- Select a Continent ------- ");
+    # Check for each suffix and add entries if valid zone data exists
+    foreach my $suffix (plugin::get_suffixes()) {
+        if (exists($zone_data->{$suffix}) && %{ $zone_data->{$suffix} }) {
+            $client->Message(257, "-[ " . quest::saylink(plugin::get_continent_by_suffix($suffix), 1));
+        }
+    }
+  }
+  if ($text =~ /^($continent_regex)$/i) {
+    my $continent = ucfirst(lc($1));
+    quest::whisper("Suffix: " . plugin::get_suffix_by_continent($continent));
+  }
+
   }
 }
 
---
+++ plugins/cata_wp_utils.pl
@@ -0,0 +1,99 @@
+my %suffix_to_pretty_name = (
+    'A' => 'Antonica',
+    'G' => 'Gunthak',
+    'O' => 'Odus',
+    'F' => 'Faydwer',
+    'K' => 'Kunark',
+    'V' => 'Velious',
+    'L' => 'Luclin',
+    'P' => 'The Planes',
+    'T' => 'Taelosia',
+    'D' => 'Discord',
+);
+
+sub get_suffixes {
+    return ('A', 'G', 'O', 'F', 'K', 'V', 'L', 'P', 'T', 'D'); 
+}
+
+sub get_continent_by_suffix {
+    my ($suffix) = @_;  
+
+    return $suffix_to_pretty_name{$suffix} || $suffix;
+}
+
+sub get_suffix_by_continent {
+    my ($continent) = @_;
+    
+    my %suffix_by_continent;
+    foreach my $suffix (keys %suffix_to_pretty_name) {
+        my $pretty_name = $suffix_to_pretty_name{$suffix};
+        $suffix_by_continent{$pretty_name} = $suffix;
+    }
+
+    return $suffix_by_continent{$continent} || $continent;
+}
+
+# Get a map of zone data for each suffix
+sub get_zone_data {
+    my ($accountID) = @_;
+    my %zone_data_by_suffix;
+
+    foreach my $suffix (get_suffixes()) {
+        my $teleport_zones = get_zone_data_for_account($accountID, $suffix);
+        $zone_data_by_suffix{$suffix} = $teleport_zones;
+    }
+
+    return \%zone_data_by_suffix;
+}
+
+# Check if a particular piece of data (by zone description) is present
+sub has_zone_entry {
+    my ($accountID, $zone_desc, $suffix) = @_;
+    my $teleport_zones = get_zone_data_for_account($accountID, $suffix);
+
+    return exists($teleport_zones->{$zone_desc});
+}
+
+# Get character's saved zone data
+sub get_zone_data_for_account {
+    my ($accountID, $suffix) = @_;
+    my $charKey = $accountID . "-TL-Account-" . $suffix;
+
+    my $charDataString = quest::get_data($charKey);
+
+    my %teleport_zones;
+    my @zone_entries = split /:/, $charDataString;
+
+    foreach my $entry (@zone_entries) {
+        my @tokens = split /,/, $entry;
+        $teleport_zones{$tokens[0]} = [@tokens[1..$#tokens]];
+    }
+
+    return \%teleport_zones;
+}
+
+# Add (or overwrite) data to teleport_zones
+# Usage:
+#    add_zone_entry(12345, "Zone4", ['data7', 'data8'], '-K');
+sub add_zone_entry {
+    my ($accountID, $zone_name, $zone_data, $suffix) = @_;
+    my $teleport_zones = get_zone_data_for_account($accountID, $suffix);
+    $teleport_zones->{$zone_name} = $zone_data;
+    set_zone_data_for_account($accountID, $teleport_zones, $suffix);
+}
+
+sub set_zone_data_for_account {
+    my ($accountID, $zone_data_hash_ref, $suffix) = @_;
+    my $charKey = $accountID . "-TL-Account-" . $suffix;
+
+    my @data_entries;
+
+    while (my ($desc, $zone_data) = each %{$zone_data_hash_ref}) {
+        my $entry = join(",", $desc, @{$zone_data});
+        push @data_entries, $entry;
+    }
+
+    my $charDataString = join(":", @data_entries);
+
+    quest::set_data($charKey, $charDataString);
+}
