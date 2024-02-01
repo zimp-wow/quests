989ee3f9267dc359717612089fc8d304246344de
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-01 09:41:05 -0600

    fix some bugs

--- global/global_player.pl
+++ global/global_player.pl
@@ -85,235 +85,210 @@ if (quest::get_data($bazkey) == "") {
     
 }
 
-$maxlvl = $client->GetBucket("CharMaxLevel"); #Code for Max Level
+	$maxlvl = $client->GetBucket("CharMaxLevel"); #Code for Max Level
 
-if (($maxlvl) == "") {
-	$client->SetBucket("CharMaxlevel", 51); #By default, on initial log in (first time) we are setting Max Level to 51.
-}
-
-if($PCClass == 1){ #War AA
-	$waraa = $client->CharacterID() . "war";
-	if (!quest::get_data($waraa)) {
-		quest::set_data($waraa, 1);
-		$client->Message(15, "You have learned Infused by Rage, Killing Spree, and Vehement Rage! Open up your AA window (Default V) to find them!");
-		$client->IncrementAA(6283); #Infused by Rage
-		$client->IncrementAA(6607); #Vehement Rage
-		$client->IncrementAA(4739); #Killing Spree
-		$client->IncrementAA(1597); #Call of Challenge		
-
+	if (($maxlvl) == "") {
+		$client->SetBucket("CharMaxlevel", 51); #By default, on initial log in (first time) we are setting Max Level to 51.
 	}
-}
 
-if($PCClass == 2){ #Clr AA
-	$clraa = $client->CharacterID() . "clr";
-	if (quest::get_data($clraa) == "") {
-    quest::set_data($clraa, 1);
-	$client->Message(15, "You have learned Twincast and Divine Arbitration! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(12652); #Twincast   
-	$client->IncrementAA(507); #Divine Arbitration
-	$client->IncrementAA(746); #Divine Avatar
-    
+	if($PCClass == 1){ #War AA
+		$waraa = $client->CharacterID() . "war";
+		if (!quest::get_data($waraa)) {
+			quest::set_data($waraa, 1);
+			$client->Message(15, "You have learned Infused by Rage, Killing Spree, and Vehement Rage! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(6283); #Infused by Rage
+			$client->IncrementAA(6607); #Vehement Rage
+			$client->IncrementAA(4739); #Killing Spree
+			$client->IncrementAA(1597); #Call of Challenge		
 
-}
-}
+		}
+	}
 
-if($PCClass == 3){ #Pal AA
-	$palaa = $client->CharacterID() . "pal";
-	if (quest::get_data($palaa) == "") {
-    quest::set_data($palaa, 1);
-	$client->Message(15, "You have learned Divine Stun and Blessing of Life! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(188); #Divine Stun
-	$client->IncrementAA(6395); #Blessing of Life  
-    
+	if($PCClass == 2){ #Clr AA
+		$clraa = $client->CharacterID() . "clr";
+			if (!quest::get_data($clraa)) {
+			quest::set_data($clraa, 1);
+			$client->Message(15, "You have learned Twincast and Divine Arbitration! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(12652); #Twincast   
+			$client->IncrementAA(507); #Divine Arbitration
+			$client->IncrementAA(746); #Divine Avatar
+			
 
-}
-}
+		}
+	}
 
-if($PCClass == 4){ #Rng AA
-	$rngaa = $client->CharacterID() . "rng";
-	if (quest::get_data($rngaa) == "") {
-    quest::set_data($rngaa, 1);
-	$client->Message(15, "You have learned Endless Quiver, Bow Mastery, Entrap, and Auspice of the Hunter! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(205); #Endless Quiver
-	$client->IncrementAA(1196); #Bow Mastery 
-	$client->IncrementAA(645); #Entrap
-	$client->IncrementAA(1345); #Auspice of the Hunter   
+	if($PCClass == 3){ #Pal AA
+		$palaa = $client->CharacterID() . "pal";
+			if (!quest::get_data($palaa)) {
+			quest::set_data($palaa, 1);
+			$client->Message(15, "You have learned Divine Stun and Blessing of Life! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(188); #Divine Stun
+			$client->IncrementAA(6395); #Blessing of Life  
+			
 
-}
-}
+		}
+	}
 
-if($PCClass == 5){ #SK AA
-	$skaa = $client->CharacterID() . "sk";
-	if (quest::get_data($skaa) == "") {
-    quest::set_data($skaa, 1);
-	$client->Message(15, "You have Mortal Coil and Explosion of Spite! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(5085); #Mortal Coil
-	$client->IncrementAA(13165); #Explosion of Spite
-    
+	if($PCClass == 4){ #Rng AA
+		$rngaa = $client->CharacterID() . "rng";
+		if (!quest::get_data($rngaa)) {
+			quest::set_data($rngaa, 1);
+			$client->Message(15, "You have learned Endless Quiver, Bow Mastery, Entrap, and Auspice of the Hunter! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(205); #Endless Quiver
+			$client->IncrementAA(1196); #Bow Mastery 
+			$client->IncrementAA(645); #Entrap
+			$client->IncrementAA(1345); #Auspice of the Hunter   
 
-}
-}
+		}
+	}
 
-if($PCClass == 6){ #Dru AA
-	$druaa = $client->CharacterID() . "dru";
-	if (quest::get_data($druaa) == "") {
-    quest::set_data($druaa, 1);
-	$client->Message(15, "You have learned Spirit of the Wood and Paralytic Spores! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(548); #Spirit of the Wood
-	$client->IncrementAA(14264); #Paralytic Spores
-	$client->IncrementAA(767); #Critical Affliction
-	$client->IncrementAA(6375); #Destructive Cascade
-    
+	if($PCClass == 5){ #SK AA
+		$skaa = $client->CharacterID() . "sk";
+			if (!quest::get_data($skaa)) {
+			quest::set_data($skaa, 1);
+			$client->Message(15, "You have Mortal Coil and Explosion of Spite! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(5085); #Mortal Coil
+			$client->IncrementAA(13165); #Explosion of Spite
+			
 
-}
-}
+		}
+	}
 
-if($PCClass == 7){ #Monk AA
-	$mnkaa = $client->CharacterID() . "mnk";
-	if (quest::get_data($mnkaa) == "") {
-    quest::set_data($mnkaa, 1);
-	$client->Message(15, "You have learned Stonewall and Crippling Strike! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(810); #Stonewall
-	$client->IncrementAA(1352); #Crippling Strike
-    
+	if($PCClass == 6){ #Dru AA
+		$druaa = $client->CharacterID() . "dru";
+			if (!quest::get_data($druaa)) {
+			quest::set_data($druaa, 1);
+			$client->Message(15, "You have learned Spirit of the Wood and Paralytic Spores! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(548); #Spirit of the Wood
+			$client->IncrementAA(14264); #Paralytic Spores
+			$client->IncrementAA(767); #Critical Affliction
+			$client->IncrementAA(6375); #Destructive Cascade
+			
 
-}
-}
+		}
+	}
 
-if($PCClass == 8){ #Bard AA
-	$brdaa = $client->CharacterID() . "brd";
-	if (quest::get_data($brdaa) == "") {
-    quest::set_data($brdaa, 1);
-	$client->Message(15, "You have learned Fading Memories and Dance of Blades! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(630); #Fading Memories
-	$client->IncrementAA(556); #Harmonious Attack I
-	$client->IncrementAA(557); #Harmonious Attack II
-	$client->IncrementAA(558); #Harmonious Attack III
-	$client->IncrementAA(559); #Harmonious Attack IV
-	$client->IncrementAA(560); #Harmonious Attack V
-	$client->IncrementAA(1110); #Dance of Blades
-	$client->IncrementAA(225); #Jam Fest I
-    
+	if($PCClass == 7){ #Monk AA
+		$mnkaa = $client->CharacterID() . "mnk";
+		if (!quest::get_data($mnkaa)) {
+			quest::set_data($mnkaa, 1);
+			$client->Message(15, "You have learned Stonewall and Crippling Strike! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(810); #Stonewall
+			$client->IncrementAA(1352); #Crippling Strike 
+		}
+	}
 
-}
-}
+	if($PCClass == 8){ #Bard AA
+		$brdaa = $client->CharacterID() . "brd";
+		if (!quest::get_data($brdaa)) {
+			quest::set_data($brdaa, 1);
+			$client->Message(15, "You have learned Fading Memories and Dance of Blades! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(630); #Fading Memories
+			$client->IncrementAA(556); #Harmonious Attack I
+			$client->IncrementAA(557); #Harmonious Attack II
+			$client->IncrementAA(558); #Harmonious Attack III
+			$client->IncrementAA(559); #Harmonious Attack IV
+			$client->IncrementAA(560); #Harmonious Attack V
+			$client->IncrementAA(1110); #Dance of Blades
+			$client->IncrementAA(225); #Jam Fest I    
+		}
+	}
 
-if($PCClass == 9){ #Rogue AA
-	$rogaa = $client->CharacterID() . "rog";
-	if (quest::get_data($rogaa) == "") {
-    quest::set_data($rogaa, 1);
-	$client->Message(15, "You have learned Chaotic Stab and Shroud of Stealth! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(287); #Chaotic Stab
-	$client->IncrementAA(605); #Shroud of Stealth
-	$client->IncrementAA(4739); #Killing Spree
-    
+	if($PCClass == 9){ #Rogue AA
+		$rogaa = $client->CharacterID() . "rog";
+		if (!quest::get_data($rogaa)) {
+			quest::set_data($rogaa, 1);
+			$client->Message(15, "You have learned Chaotic Stab and Shroud of Stealth! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(287); #Chaotic Stab
+			$client->IncrementAA(605); #Shroud of Stealth
+			$client->IncrementAA(4739); #Killing Spree  
+		}
+	}
 
-}
-}
+	if($PCClass == 10){ #Shaman AA
+		$shmaa = $client->CharacterID() . "shm";
+		if (!quest::get_data($shmaa)) {
+			quest::set_data($shmaa, 1);
+			$client->Message(15, "You have learned Group Shrink, Summon Companion and Ancestral Aid! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(10957); #Group Shrink
+			$client->IncrementAA(1327); #Ancestral Aid
+			$client->IncrementAA(8227); #Summon Companion	
+		}
+	}
 
-if($PCClass == 10){ #Shaman AA
-	$shmaa = $client->CharacterID() . "shm";
-	if (quest::get_data($shmaa) == "") {
-    quest::set_data($shmaa, 1);
-	$client->Message(15, "You have learned Group Shrink, Summon Companion and Ancestral Aid! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(10957); #Group Shrink
-	$client->IncrementAA(1327); #Ancestral Aid
-	$client->IncrementAA(8227); #Summon Companion
-    
+	if($PCClass == 11){ #Necromancer AA
+		$necaa = $client->CharacterID() . "nec";
+		if (!quest::get_data($necaa)) {
+			quest::set_data($necaa, 1);
+			$client->Message(15, "You have learned Death's Fury, Pet Affinity, Summon Companion and Pestilent Paralysis! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(767); #Critical Affliction
+			$client->IncrementAA(6375); #Destructive Cascade
+			$client->IncrementAA(734); #Pet Affinity
+			$client->IncrementAA(12770); #Pestilent Paralysis
+			$client->IncrementAA(8227); #Summon Companion
+		}
+	}
 
-}
-}
+	if($PCClass == 12){ #Wizard AA
+		$wizaa = $client->CharacterID() . "wiz";
+		if (!quest::get_data($wizaa)) {
+			quest::set_data($wizaa, 1);
+			$client->Message(15, "You have learned Improved Familiar, Harvest of Druzzil, and Arcane Overkill! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(155); #Improved Familiar
+			$client->IncrementAA(516); #Harvest of Druzzil
+			$client->IncrementAA(5295); #Arcane Overkill
+		}
+	}
 
-if($PCClass == 11){ #Necromancer AA
-	$necaa = $client->CharacterID() . "nec";
-	if (quest::get_data($necaa) == "") {
-    quest::set_data($necaa, 1);
-	$client->Message(15, "You have learned Death's Fury, Pet Affinity, Summon Companion and Pestilent Paralysis! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(767); #Critical Affliction
-	$client->IncrementAA(6375); #Destructive Cascade
-	$client->IncrementAA(734); #Pet Affinity
-	$client->IncrementAA(12770); #Pestilent Paralysis
-	$client->IncrementAA(8227); #Summon Companion
+	if($PCClass == 13){ #Mage AA
+		$magaa = $client->CharacterID() . "mag";
+		if (!quest::get_data($magaa)) {
+			quest::set_data($magaa, 1);
+			$client->Message(15, "You have learned Companion's Fury, Pet Affinity, Summon Companion and Host in the Shell! Open up your AA window (Default V) to find it!");
+			$client->IncrementAA(8201); #Companion's Fury
+			$client->IncrementAA(734); #Pet Affinity
+			$client->IncrementAA(8342); #Host in the Shell
+			$client->IncrementAA(8227); #Summon Companion
+		}
+	}
 
-    
+	if($PCClass == 14){ #Enchanter AA
+		$encaa = $client->CharacterID() . "enc";
+		if (!quest::get_data($encaa)) {
+			quest::set_data($encaa, 1);
+			$client->Message(15, "You have learned Permanent Illusion, Project Illusion, Phantasmic Reflex, Summon Companion and Animation Empathy! Open up your AA window (Default V) to find them!");
+			$client->IncrementAA(158); #Permanent Illusion
+			$client->IncrementAA(643); #Project Illusion
+			$client->IncrementAA(10551); #Phantasmic Reflex
+			$client->IncrementAA(580); #Animation Empathy 1
+			$client->IncrementAA(581); #Animation Empathy 2
+			$client->IncrementAA(582); #Animation Empathy 3
+			$client->IncrementAA(734); #Pet Affinity
+			$client->IncrementAA(8227); #Summon Companion
+		}
+	}
 
-}
-}
+	if($PCClass == 15){ #Beastlord AA
+		$blaa = $client->CharacterID() . "bl";
+		if (!quest::get_data($blaa)) {
+			quest::set_data($blaa, 1);
+			$client->Message(15, "You have learned Chameleon Strike, Bite of the Asp, and Summon Companion! Open up your AA window (Default V) to find it!");
+			$client->IncrementAA(11080); #Chameleon Strike
+			$client->IncrementAA(6984); #Bite of the Asp
+			$client->IncrementAA(734); #Pet Affinity
+			$client->IncrementAA(8227); #Summon Companion
+		}
+	}
 
-if($PCClass == 12){ #Wizard AA
-	$wizaa = $client->CharacterID() . "wiz";
-	if (quest::get_data($wizaa) == "") {
-    quest::set_data($wizaa, 1);
-	$client->Message(15, "You have learned Improved Familiar, Harvest of Druzzil, and Arcane Overkill! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(155); #Improved Familiar
-	$client->IncrementAA(516); #Harvest of Druzzil
-	$client->IncrementAA(5295); #Arcane Overkill
-
-
-    
-
-}
-}
-
-if($PCClass == 13){ #Mage AA
-	$magaa = $client->CharacterID() . "mag";
-	if (quest::get_data($magaa) == "") {
-    quest::set_data($magaa, 1);
-	$client->Message(15, "You have learned Companion's Fury, Pet Affinity, Summon Companion and Host in the Shell! Open up your AA window (Default V) to find it!");
-	$client->IncrementAA(8201); #Companion's Fury
-	$client->IncrementAA(734); #Pet Affinity
-	$client->IncrementAA(8342); #Host in the Shell
-	$client->IncrementAA(8227); #Summon Companion
-
-    
-
-}
-}
-
-if($PCClass == 14){ #Enchanter AA
-	$encaa = $client->CharacterID() . "enc";
-	if (quest::get_data($encaa) == "") {
-    quest::set_data($encaa, 1);
-	$client->Message(15, "You have learned Permanent Illusion, Project Illusion, Phantasmic Reflex, Summon Companion and Animation Empathy! Open up your AA window (Default V) to find them!");
-	$client->IncrementAA(158); #Permanent Illusion
-	$client->IncrementAA(643); #Project Illusion
-	$client->IncrementAA(10551); #Phantasmic Reflex
-	$client->IncrementAA(580); #Animation Empathy 1
-	$client->IncrementAA(581); #Animation Empathy 2
-	$client->IncrementAA(582); #Animation Empathy 3
-	$client->IncrementAA(734); #Pet Affinity
-	$client->IncrementAA(8227); #Summon Companion
-
-    
-
-}
-}
-
-if($PCClass == 15){ #Beastlord AA
-	$blaa = $client->CharacterID() . "bl";
-	if (quest::get_data($blaa) == "") {
-    quest::set_data($blaa, 1);
-	$client->Message(15, "You have learned Chameleon Strike, Bite of the Asp, and Summon Companion! Open up your AA window (Default V) to find it!");
-	$client->IncrementAA(11080); #Chameleon Strike
-	$client->IncrementAA(6984); #Bite of the Asp
-	$client->IncrementAA(734); #Pet Affinity
-	$client->IncrementAA(8227); #Summon Companion
-
-    
-
-}
-}
-
-if($PCClass == 16){ #Zerker AA
-	$zerkaa = $client->CharacterID() . "zerk";
-	if (quest::get_data($zerkaa) == "") {
-    quest::set_data($zerkaa, 1);
-	$client->Message(15, "You have learned ! Open up your AA window (Default V) to find it!");
-	$client->IncrementAA(4739); #Killing Spree
-	$client->IncrementAA(258); #Rampage    
-}
-}
-
+	if($PCClass == 16){ #Zerker AA
+		$zerkaa = $client->CharacterID() . "zerk";
+			if (!quest::get_data($zerkaa)) {
+			quest::set_data($zerkaa, 1);
+			$client->Message(15, "You have learned ! Open up your AA window (Default V) to find it!");
+			$client->IncrementAA(4739); #Killing Spree
+			$client->IncrementAA(258); #Rampage    
+		}
+	}
 }
 
 sub EVENT_SAY {
@@ -385,40 +360,7 @@ sub EVENT_ZONE {
 			$client->Message(15, "Your maxmimum level has been set to 70.");
 		}
 	}
-
-
-#	if (quest::get_data($AM11) == "") {
-#	$client->IncrementAA(199); #AM1
- #   quest::set_data($AM11, 1);
-#}
-
-
-#	if (quest::get_data($AM22) == "") {
-#	$client->IncrementAA(200); #AM1
- #   quest::set_data($AM22, 1);
-#}
-
 }
-
-	#if ($client->GetPetID()) {
-	#	$PetID = $entity_list->GetMobByID($client->GetPetID());
-	#	$PetID->Kill();
-	#}
-	#if ($name eq "TurmoilToad") {
-	#	quest::playerrace(27);
-	#	quest::playersize(4);
-	#}
-	#if ($name eq "Fippy") {
-	#	quest::playerrace(39);
-	#	quest::playersize(5);
-	#	quest::playertexture(2);
-	#}
-	#if ($name eq "Sergeant") {
-	#	quest::playerrace(44);
-	#	quest::playersize(6);
-	#	quest::playertexture(1);
-	#	quest::playerfeature("helm", 1);
-	#}
 
 sub EVENT_DISCOVER_ITEM {
 	$itemname = quest::getitemname($itemid);
