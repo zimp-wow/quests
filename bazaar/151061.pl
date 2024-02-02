0d1d4edaff8cbe2fe7817a1caec4f6f56f8caae9
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-02 01:20:47 -0600

    wtf

--- bazaar/151061.pl
+++ bazaar/151061.pl
@@ -18,7 +18,7 @@ if ($client->GetGM()) {
             my $races = get_races();
             foreach my $id (sort { $a <=> $b } keys %$races) {
                 my $race_name = $races->{$id};
-                $client->Message(257, "-[ " . quest::saylink("Change race to $race_name", 1, "RACECHANGE:".$id) . " ]");
+                $client->Message(257, "-[ " . quest::saylink("RACECHANGE:".$id, 1, $race_name));
             }
         }
 
