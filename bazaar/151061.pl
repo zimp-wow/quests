e8bdef066687b07da9603c674f3af64c838f87ef
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-02 01:42:16 -0600

    test

--- bazaar/151061.pl
+++ bazaar/151061.pl
@@ -2,19 +2,19 @@
 #(Body Modification)
 
 my $race_change_cost = 5;
+my $sex_change_cost = 5;
 
 sub EVENT_SAY {
 if ($client->GetGM()) {
-    if ($text=~/hail/i) {
-        
-        my $sex;
+    if ($text=~/hail/i) {        
+        my $sex_word;
         if ($client->GetGender()) {
-            quest::debug("true");
+            $sex_word = "masculinity";
         } else {
-
+            $sex_word = "femininity";
         }
 
-        quest::say("Greetings, $name. Do you seek perfection? Are you [unhappy with your form]? Perhaps, instead you desire to [bestow a nickname] upon your companion?");
+        quest::say("Greetings, $name. Do you seek perfection? Are you [unhappy with your form]? Are you interested in embracing [$sex_word]? Perhaps, instead you simply desire a [new identity] altogether?");
     }
 
     elsif ($text=~/unhappy with your form/i) {
@@ -44,6 +44,19 @@ if ($client->GetGM()) {
             } else {
                 $client->Message(13, "An error occurred. Unable to change race to ID $race_id.");
             }
+        } else {
+            quest::say("Sadly, $name, you do not have sufficient Echoes of Memory to properly anchor yourself for this change. Perhaps you will return later.");
+        }
+    }
+    
+    elsif ($text == $sex_word) {
+        quest::say("Just [". quest::saylink("SEXCHANGE", 1, "say the word") ."], and for the price of a mere ". plugin::num2en($sex_change_cost) ." [". plugin::EOMLink() ."], I will adjust your form.");
+        $client->Message(15, "WARNING: You will disconnect immediately upon changing your sex.");
+    }
+
+    elsif ($text == "SEXCHANGE") {
+        if (plugin::SpendEOM($client, $sex_change_cost)) {
+            quest::permagender(!$client->GetGender());
         } else {
             quest::say("Sadly, $name, you do not have sufficient Echoes of Memory to properly anchor yourself for this change. Perhaps you will return later.");
         }
