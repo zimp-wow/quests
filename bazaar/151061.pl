9fd0ef3ab52b3ebfb6c0db9d59495d84f14ed1e0
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-02 00:30:33 -0600

    Initial work on racechange npc
    oops
    
    testing
    
    test
    
    test
    
    test
    
    lol
    
    f
    
    f
    
    f

---
+++ bazaar/151061.pl
@@ -0,0 +1,46 @@
+#Sulentia, Polymorphist
+#(Body Modification)
+
+my $race_change_cost = 5;
+
+sub EVENT_SAY {
+if ($client->GetGM()) {
+    if ($text=~/hail/i) {
+        quest::say("Greetings, $name. Do you seek perfection? Are you [unhappy with your form]? Perhaps, instead you desire to [bestow a nickname] upon your companion?");
+    }
+
+    elsif ($text=~/unhappy with your form/i) {
+        quest::say("Just so. If you can properly anchor your memories - perhaps with ". plugin::num2en($race_change_cost) ." [". plugin::EOMLink() ."], I can adjust your form.
+                    Be warned, this process is quite traumatic, and you will immediately black out.");
+        if (plugin::GetEOM($client) > $race_change_cost) {
+            $client->Message(15, "Warning. You will disconnect immediately upon selecting a new race.");
+            $client->Message(257, " ------- Select a Race ------- ");
+            my $races = get_races();
+            foreach my $id (sort { $a <=> $b } keys %$races) {
+                my $race_name = $races{$race_id};
+                $client->Message(257, "-[ " . quest::saylink("Change race to $race_name", 1, $race_name) . " ]");
+            }
+        }
+    }
+}
+}
+
+sub get_races {
+    return {
+        1   => 'Human',
+        2   => 'Barbarian',
+        3   => 'Erudite',
+        4   => 'Wood Elf',
+        5   => 'High Elf',
+        6   => 'Dark Elf',
+        7   => 'Half Elf',
+        8   => 'Dwarf',
+        9   => 'Troll',
+        10  => 'Ogre',
+        11  => 'Halfling',
+        12  => 'Gnome',
+        128 => 'Iksar',
+        130 => 'Vah Shir',
+        330 => 'Froglok',
+    };
+}
--- plugins/cata_donate_utils.pl
+++ plugins/cata_donate_utils.pl
@@ -1,4 +1,5 @@
 my $eom_id = 6;
+my $eom_item_id = 46779;
 my $eom_log = "total-eom-spend";
 
 # Spend EOM. Returns true on success, false on failure
@@ -8,15 +9,61 @@ sub SpendEOM {
     my $eom_available = $client->GetAlternateCurrencyValue($eom_id);
 
     if ($eom_available >= $amount) {
-        $client->SetAlternateCurrencyValue($eom_id, $eom_available - $amount); # Use $amount instead of hardcoding 5
-        $client->Message(15, "You have SPENT $amount [".quest::varlink(46779)."]."); # Reflect the dynamic $amount
-
+        $client->SetAlternateCurrencyValue($eom_id, $eom_available - $amount);
+        $client->Message(15, "You have spent $amount [".quest::varlink($eom_item_id)."].");
         if (!$client->GetGM()) {
-            my $current_eom_spend = quest::get_data($eom_log) || 0; # Defaults to 0 if not previously set
-            quest::set_data($eom_log, $current_eom_spend + $amount);
+            quest::set_data($eom_log, (quest::get_data($eom_log) || 0) + $amount);
         }
-
         return 1;
     }
     return 0;
+}
+
+sub GetEOM {
+    my ($client) = @_;
+
+    return $client->GetAlternateCurrencyValue($eom_id);
+}
+
+sub EOMLink {
+    return quest::varlink($eom_item_id);
+}
+
+sub num2en {
+    my $number = shift;
+
+    return "zero" if $number == 0; # Handle 0 explicitly
+    return "one thousand" if $number == 1000; # Special case for 1000
+
+    my %map = (
+        1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five",
+        6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten",
+        11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen",
+        15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen",
+    );
+
+    my %tens_map = (
+        2 => "twenty", 3 => "thirty", 4 => "forty", 5 => "fifty",
+        6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety",
+    );
+
+    my $word = '';
+
+    if ($number >= 100) {
+        my $hundreds = int($number / 100);
+        $word .= $map{$hundreds} . " hundred";
+        $number %= 100; # Reduce number to remainder for further processing
+        $word .= " and " if $number > 0; # Add 'and' if there's more to come
+    }
+
+    if ($number >= 20) {
+        my $tens = int($number / 10);
+        $word .= $tens_map{$tens};
+        $number %= 10; # Reduce number to remainder for ones place
+        $word .= "-" if $number > 0; # Add hyphen for numbers like "twenty-one"
+    }
+
+    $word .= $map{$number} if $number > 0 && exists $map{$number};
+
+    return $word;
 }
