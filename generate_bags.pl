ca750c91ab0832dcb13addef369860308e4ef8bd
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-08 12:00:00 -0600

    hrm?

--- generate_bags.pl
+++ generate_bags.pl
@@ -62,9 +62,9 @@ sub duplicate_and_modify_items {
             my $isth = $dbh->prepare($sql) or die "Failed to prepare insert: $DBI::errstr";
             $isth->execute() or die "Failed to execute insert: $DBI::errstr";
         }
-    } else {
-        print "Error";
     }
+
+    
 
     $sth->finish();
     $dbh->disconnect();
