fd00c88ec78b243a8aafab279e2cb547437a2476
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-10 13:16:34 -0500

    fix iksar and delf masks

--- bazaar/Shin_Noir.pl
+++ bazaar/Shin_Noir.pl
@@ -15,12 +15,10 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM 
 {    
-    if (plugin::check_handin(\%itemcount, 72469 => 1)  || # Rose Colored Guise of the Deceiver
-        #plugin::check_handin(\%itemcount, 72469 => 1) || # Rose Colored Guise of the Deceiver
-        plugin::check_handin(\%itemcount, 82469 => 1) || # Apocryphal Guise of the Deceiver
-        #plugin::check_handin(\%itemcount, 2472 => 1)  || # Mask of Deception
-        plugin::check_handin(\%itemcount, 72472 => 1) || # Rose Colored Mask of Deception
-        plugin::check_handin(\%itemcount, 82472 => 1))   # Apocryphal Mask of Deception
+    if (plugin::check_handin(\%itemcount, 1002469 => 1)  || # Rose Colored Guise of the Deceiver
+        plugin::check_handin(\%itemcount, 2002469 => 1) || # Apocryphal Guise of the Deceiver
+        plugin::check_handin(\%itemcount, 1002472 => 1) || # Rose Colored Mask of Deception
+        plugin::check_handin(\%itemcount, 2002472 => 1))   # Apocryphal Mask of Deception
     {
         quest::say("Mmmph!!.. *Pop!!* Ouch, my thumb!! Here you are.");
         quest::say("(Log out to see your changes)");
