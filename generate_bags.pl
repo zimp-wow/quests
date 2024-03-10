bc2c04897d248e21e3cd98acb6ede4c9713f376c
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-09 20:14:25 -0600

    test

--- generate_bags.pl
+++ generate_bags.pl
@@ -51,8 +51,10 @@ sub duplicate_and_modify_items {
 
             $row{id} += 1000000 * $multiplier;  
             $row{Name} = ($multiplier == 1 ? "Rose Colored " : "Apocryphal ") . $original_row->{Name};
-            $row{bagwr} = min(100, max($row{bagwr}, $new_value, $increment)); 
-            $row{bagslots} += 5 * $multiplier; 
+
+
+            $row{bagwr} = min(100,  $row{bagwr} + (10 * (max(1,($row{bagsize} - 4)/2)) * $multiplier)); 
+            $row{bagslots} +=  $row{bagslots} + (4 * (max(1,($row{bagsize} - 4)/2)) * $multiplier); 
 
             my $columns = join(",", map { $dbh->quote_identifier($_) } keys %row);
             my $values  = join(",", map { $dbh->quote($row{$_}) } keys %row);
