9b276f933180f712e00126e64d6abb8285c91d64
RetributionEQ <info@retributioneq.com>
2024-03-15 20:43:31 -0400

    Add both versions jic

--- poinnovation/#Chronographer_Muon.pl
+++ poinnovation/#Chronographer_Muon.pl
@@ -1,5 +1,5 @@
 sub EVENT_SAY {
-	if (plugin::check_hasitem($client, 29165) || plugin::check_hasitem($client, 1029165) || plugin::check_hasitem($client, 2029165)) {
+	if (plugin::check_hasitem($client, 29165) || plugin::check_hasitem($client, 1029165) || plugin::check_hasitem($client, 2029165) || plugin::check_hasitem($client, 18637) || plugin::check_hasitem($client, 1018637) || plugin::check_hasitem($client, 2018637)) {
 		if ($text=~/Hail/i) {
 			quest::say("Acquisition of power completed. Would you like to be transported to the time-projection chamber?");
 		}
