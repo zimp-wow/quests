a7a0c5092aeec108d41fe02710b8fa69b148b9de
RetributionEQ <info@retributioneq.com>
2024-03-16 20:23:20 -0400

    Reverting all of it

--- kodtaz/Tublik_Narwethar.pl
+++ kodtaz/Tublik_Narwethar.pl
@@ -230,8 +230,8 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  if (plugin::check_hasitem($client, 60252)) {
-    if (plugin::check_handin(\%itemcount, 60174 => 1)){
+  if (plugin::check_handin(\%itemcount, 60174 => 1)){
+    if (plugin::check_hasitem($client, 60252)) {
       quest::say("You have done great things for us, but your journey is not yet over.");  #need live text
       $client->Message(4, "Finished! - You've recovered the Sliver of the High Temple! Congratulations!");
       $client->Message(4, "You feel the magic protecting Qvic has softened.");
