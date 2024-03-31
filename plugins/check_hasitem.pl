fa5073db0983e283d718025f9649ae084269a0b9
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-31 15:17:35 -0500

    update has_item plugin to be version-agnostic

--- plugins/check_hasitem.pl
+++ plugins/check_hasitem.pl
@@ -6,12 +6,12 @@ sub check_hasitem {
 
 	my @slots = (0..30, 251..340, 2000..2023, 4010..6009, 6210..11009, 11010..11409, 9999);
 	foreach $slot (@slots) {
-		if ($client->GetItemIDAt($slot) == $itemid) {
+		if ($client->GetItemIDAt($slot) % 1000000 == $itemid % 1000000) {
 			return 1;
 		}
 
 		for ($i = 0; $i < 5; $i++) {
-			if ($client->GetAugmentIDAt($slot, $i) == $itemid) {
+			if ($client->GetAugmentIDAt($slot, $i) % 1000000 == $itemid % 1000000) {
 				return 1;
 			}
 		}
