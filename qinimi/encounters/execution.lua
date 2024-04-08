5963e8391521061837ca2c695f881018a0fd4dfc
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-08 14:56:47 -0500

    stone of entry?

--- qinimi/encounters/execution.lua
+++ qinimi/encounters/execution.lua
@@ -112,9 +112,8 @@ end
 function Trigger_proximity_say(e)
 	if e.message:findi("i wish to enter") then
 		local client = e.other;
-		if client:GetItemIDAt(Slot.Cursor) % 1000000 == 67415 then --stone of entry
+		if client:GetItemIDAt(Slot.Cursor) % 1000000 == 67415 or client:HasItem(67415) then --stone of entry
 			if event_flag == 0 then
-
 				-- Move the Player and their Group to the trial room.
 				local trial_group = e.other:GetGroup();
 				if trial_group ~= nil and trial_group.valid then
