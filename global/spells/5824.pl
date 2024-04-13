991249bcffe072a4b1f6e2405a1ea73d6a287391
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-13 13:37:38 -0500

    ok that makes more sense

--- global/spells/5824.pl
+++ global/spells/5824.pl
@@ -14,23 +14,17 @@ sub EVENT_SPELL_EFFECT_CLIENT {
         $client->SetBucket("Return-Instance", $instanceid);
     } else {
         quest::debug("We are in Bazaar");
-        my $ReturnX = $client->GetBucket("Return-X");
-        my $ReturnY = $client->GetBucket("Return-Y");
-        my $ReturnZ = $client->GetBucket("Return-Z");
-        my $ReturnH = $client->GetBucket("Return-H");
-        my $ReturnZone = $client->GetBucket("Return-Zone");
-        my $ReturnZone = $client->GetBucket("Return-Instance") || 0;
+        my $ReturnX = $client->GetBucket("Return-X") || undef;
+        my $ReturnY = $client->GetBucket("Return-Y") || undef;
+        my $ReturnZ = $client->GetBucket("Return-Z") || undef;
+        my $ReturnH = $client->GetBucket("Return-H") || undef;
+        my $ReturnZone = $client->GetBucket("Return-Zone") || undef;
+        my $ReturnInstance = $client->GetBucket("Return-Instance") || 0;
 
-        if ($ReturnX && $ReturnY && $ReturnZ && $ReturnH && $ReturnZone) {
-            quest::debug("Found a return location");
-            if ($ReturnInstance) {
-                $client->MovePCInstance($ReturnZone, $ReturnInstance, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);
-            } else {
-                $client->MovePC($ReturnZone, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);
-            }            
-        } else {
-            quest::debug("Debug string $ReturnX $ReturnY $ReturnZ $ReturnH $ReturnZone $ReturnZone");
-            
+        if (defined($ReturnX) && defined($ReturnY) && defined($ReturnZ) && defined($ReturnH) && defined($ReturnZone)) {
+            quest::debug("Found a return location");            
+            $client->MovePCInstance($ReturnZone, $ReturnInstance, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);            
+        } else {            
             quest::debug("Returning to default location");
             plugin::move_startzone();
             return -1;
