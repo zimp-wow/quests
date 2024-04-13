d7827d00042b48ddbc6305cb07ea88926fe38dd0
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-13 13:22:10 -0500

    helps to not be invulnerable.

--- global/spells/5824.pl
+++ global/spells/5824.pl
@@ -12,6 +12,25 @@ sub EVENT_SPELL_EFFECT_CLIENT {
         $client->SetBucket("Return-H", $client->GetHeading());
         $client->SetBucket("Return-Zone", $zoneid);
         $client->SetBucket("Return-Instance", $instanceid);
+    } else {
+        quest::debug("We are in Bazaar");
+        my $ReturnX = $client->GetBucket("Return-X");
+        my $ReturnY = $client->GetBucket("Return-Y");
+        my $ReturnZ = $client->GetBucket("Return-Z");
+        my $ReturnH = $client->GetBucket("Return-H");
+        my $ReturnZone = $client->GetBucket("Return-Zone");
+        my $ReturnInstance = $client->GetBucket("Return-Instance") || 0;
+
+        if ($ReturnX && $ReturnY && $ReturnZ && $ReturnH) {
+            if ($ReturnInstance) {
+                $client->MovePCInstance($ReturnZone, $ReturnInstance, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);
+            } else {
+                $client->MovePC($ReturnZone, $ReturnX, $ReturnY, $ReturnZ, $ReturnH);
+            }            
+        } else {
+            plugin::move_startzone();
+            return -1;
+        }
     }
 }
 
