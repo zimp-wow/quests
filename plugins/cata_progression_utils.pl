033c9cf249f93ed50ff3523571076396f521f723
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-16 15:39:32 -0500

    reduce debug spam for solved issues

--- plugins/cata_progression_utils.pl
+++ plugins/cata_progression_utils.pl
@@ -333,7 +333,7 @@ sub is_stage_complete {
     my ($client, $stage, $inform) = @_;
     $inform //= 0; # Set to 0 if not defined
 
-    quest::debug("Checking if stage is complete: $stage");
+    #quest::debug("Checking if stage is complete: $stage");
 
     # Return false if the stage is not valid
     unless (exists $VALID_STAGES{$stage}) {
@@ -341,7 +341,7 @@ sub is_stage_complete {
         return 0;
     }
 
-    quest::debug("Valid Stage: $stage");
+    #quest::debug("Valid Stage: $stage");
 
     # Check prerequisites
     foreach my $prerequisite (@{$STAGE_PREREQUISITES{$stage}}) {
@@ -350,17 +350,17 @@ sub is_stage_complete {
         my %objective_progress = map { lc($_) => $raw_objective_progress{$_} } keys %raw_objective_progress;
 
         unless ($objective_progress{$prerequisite}) {
-            quest::debug("Prerequisite not met: $prerequisite");
+            #quest::debug("Prerequisite not met: $prerequisite");
             if ($inform) {
                  $client->Message(263, "You are not yet ready to experience that memory.");
             }
             return 0;
         }
-        quest::debug("Prerequisite met: $prerequisite");
+        #quest::debug("Prerequisite met: $prerequisite");
     }
 
     # If all prerequisites are met
-    quest::debug("All prerequisites for stage $stage have been met");
+    #quest::debug("All prerequisites for stage $stage have been met");
     return 1;
 }
 
