e39d17d00287b46dbfe86e216b50348516ea41e2
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-15 21:27:58 -0500

    refactor LoadMysql to use content db if it exists

--- plugins/MySQL.pl
+++ plugins/MySQL.pl
@@ -1,36 +1,50 @@
-#::: Akkadius
-#::: Description: These plugins are MySQL loaders in use with Perl DBI
-#::: For plug and play, LoadMysql will load your database credentials from eqemu_config.json
+use DBI;
+use JSON;
 
-sub LoadMysql {	
-	use DBI;
-	use DBD::mysql;
-	use JSON;
-	
-    	my $json = new JSON();
+sub LoadMysql {
+    my $config = load_config("eqemu_config.json");
+    
+    # Attempt to connect using 'content_database'
+    my $connect = try_connect($config, "content_database");
+    
+    # If connection to 'content_database' fails, try 'database'
+    if (!$connect) {
+        $connect = try_connect($config, "database");
+    }
 
-	#::: Load Config
-	my $content;
+    return $connect;
+}
+
+sub load_config {
+    my $config_file = shift;
 	open(my $fh, '<', "eqemu_config.json") or die "cannot open file $filename"; {
 		local $/;
 		$content = <$fh>;
 	}
 	close($fh);
+    return JSON->new->decode($content);
+}
 
-	#::: Decode
-	$config = $json->decode($content);
+sub try_connect {
+    my ($config, $label) = @_;
+    return unless exists $config->{"server"}{$label};
+    
+    my $db_config = $config->{"server"}{$label};
+    my @required_keys = qw(db host username password);
+    return unless all_keys_exist($db_config, @required_keys);
+    
+    my $dsn = "dbi:mysql:dbname=$db_config->{db};host=$db_config->{host};port=3306";
+    my $connect = DBI->connect($dsn, $db_config->{username}, $db_config->{password}, { RaiseError => 0, PrintError => 0 });
+    
+    return $connect if $connect;
+    warn "Connection attempt failed for $label: $DBI::errstr";
+    return;
+}
 
-	#::: Set MySQL Connection vars
-	$db = $config->{"server"}{"database"}{"db"};
-	$host = $config->{"server"}{"database"}{"host"};
-	$user = $config->{"server"}{"database"}{"username"};
-	$pass = $config->{"server"}{"database"}{"password"};
-
-	#::: Map DSN
-	$dsn = "dbi:mysql:$db:$host:3306";
-	
-	#::: Connect and return
-	$connect = DBI->connect($dsn, $user, $pass);
-	
-	return $connect;
+sub all_keys_exist {
+    my ($hash_ref, @keys) = @_;
+    foreach my $key (@keys) {
+        return 0 unless defined $hash_ref->{$key};
+    }
+    return 1;
 }
