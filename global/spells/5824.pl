57c308d767fc9a44abcf28266e3a1ad8b2b8721e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-23 01:36:09 -0500

    fix coordinate ordering

--- global/spells/5824.pl
+++ global/spells/5824.pl
@@ -13,7 +13,7 @@ sub EVENT_SPELL_EFFECT_CLIENT {
         $client->SetBucket("Return-Zone", $zoneid);
         $client->SetBucket("Return-Instance", $instanceid);
 
-        $client->MovePCInstance(151, 0, -834, 185, 4, -125);
+        $client->MovePCInstance(151, 0, 185, -834, 4, -125);
     } else {
         quest::debug("We are in Bazaar");
         my $ReturnX = $client->GetBucket("Return-X");
