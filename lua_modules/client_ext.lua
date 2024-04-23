24b9f1391e9ca9a10fd2e9c85927c7959e156586
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-23 00:46:39 -0500

    test

--- lua_modules/client_ext.lua
+++ lua_modules/client_ext.lua
@@ -92,10 +92,11 @@ function Client:Class()
 	end
 end
 
---function Client:HasClass(classID)
---   	local bitmask = self:GetClassesBitmask()
---	return ((bitmask & (1 << (classID - 1))) ~= 0)
---end
+function Client:HasClass(classID)
+	local bit = require("bit")
+    local bitmask = self:GetClassesBitmask()
+    return (bit.band(bitmask, bit.lshift(1, classID - 1)) ~= 0)
+end
 
 function Client:Race()
 	local race = self:GetRace();
