e0a6dac9b21255e182a8097905cbb85162bd6f7b
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-23 00:47:12 -0500

    remove debug

--- poknowledge/Derin_O-Danos.lua
+++ poknowledge/Derin_O-Danos.lua
@@ -4,5 +4,4 @@ function event_say(e)
 		if(e.message:findi("hail")) then
 			e.self:Say("Welcome, traveler! If you are in need of supplies to brew potions, then you have come to the right man! Please, search my wares and perhaps you may find what you need.");
 		end
-		eq.debug(_VERSION)
 end
