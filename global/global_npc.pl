40c6e6047fea1d8f8ef29d4ec4ee5b50671d237e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-29 03:51:58 -0500

    fix bnpe

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -152,10 +152,17 @@ sub EVENT_DAMAGE_GIVEN
     # Special aggro events for player pets; if they are not taunting then add their owner to any
     # mob that they attack's aggro list. If they are taunting, then give them some bonus aggro.
     if ($npc->IsPet() && $npc->GetOwner()->IsClient()) {
+       
         if ($npc->IsTaunting()) {
-            $entity_list->GetMobByID($entity_id)->AddToHateList($npc->GetOwner());
+            my $ent = $entity_list->GetMobByID($entity_id);
+            if ($ent) {
+                $ent->AddToHateList($npc->GetOwner())
+            }
         } else {
-            $entity_list->GetMobByID($entity_id)->AddToHateList($npc, 100);
+            my $ent = $entity_list->GetMobByID($entity_id);
+            if ($ent) {
+                $ent->AddToHateList($npc, 100);
+            }
         }
     }
 }
