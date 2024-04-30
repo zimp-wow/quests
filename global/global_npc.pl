6ed0c8273a49bc3fc4b985361886113f8e6d1a44
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-30 02:23:33 -0500

    test

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -116,7 +116,7 @@ sub EVENT_DEATH_COMPLETE {
 }
 
 sub EVENT_TICK {    
-    CHECK_CHARM_STATUS();
+    #CHECK_CHARM_STATUS();
     
     # TODO: Gate this behind your owner actually having an eligible pet bag
     if (quest::get_rule("Custom:MulticlassingEnabled") ne "false") {  
@@ -136,6 +136,10 @@ sub EVENT_AGGRO {
 
 sub EVENT_SPELL_FADE {
     CHECK_CHARM_STATUS();
+}
+
+sub EVENT_DEATH {
+    CHECK_CHARM_FADE();
 }
 
 sub EVENT_SPAWN {
@@ -360,17 +364,22 @@ sub CHECK_CHARM_STATUS
         my $data = @inventory ? join(",", @inventory) : "EMPTY";
         plugin::SEV($npc, "is_charmed", $data);
 
-    } elsif (!$npc->Charmed() && plugin::REV($npc, "is_charmed")) {
+    }
+}
+
+sub CHECK_CHARM_FADE
+{
+     if (!$npc->Charmed() && plugin::REV($npc, "is_charmed")) {
         
         my $data = plugin::REV($npc, "is_charmed");
         my @inventory = split(",", $data);
 
         my @lootlist = $npc->GetLootList();
-        while (@lootlist) { # While lootlist has elements
+        while (@lootlist) {
             foreach my $item_id (@lootlist) {
                 $npc->RemoveItem($item_id);
             }
-            @lootlist = $npc->GetLootList(); # Update the lootlist after removing items
+            @lootlist = $npc->GetLootList();
         }
 
         foreach my $item (@inventory) {
