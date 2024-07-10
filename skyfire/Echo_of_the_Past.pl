d0aee5ac92e7c8c566204725096f60bc5c4d9d04
RetributionEQ <info@retributioneq.com>
2024-07-10 12:49:58 -0400

    I still want to see skyshrine mountains...

--- skyfire/Echo_of_the_Past.pl
+++ skyfire/Echo_of_the_Past.pl
@@ -1,7 +1,7 @@
-my $expedition_name = "Skyshrine Mountains";
+my $expedition_name = "Skyfire Mountains";
 my $min_players     = 1;
 my $max_players     = 72;
-my $dz_zone         = "skyshrine";
+my $dz_zone         = "skyfire";
 
 sub EVENT_SAY {
   plugin::OfferStandardInstance($expedition_name, $min_players, $max_players, $dz_zone);
