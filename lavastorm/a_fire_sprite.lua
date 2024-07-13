f6fb4b510602275bbeb115e79b8ab1a29facbd47
RetributionEQ <info@retributioneq.com>
2024-07-13 10:48:48 -0400

    Better location for Eejag

--- lavastorm/a_fire_sprite.lua
+++ lavastorm/a_fire_sprite.lua
@@ -2,7 +2,7 @@ function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Emote("ignores you.");
 	elseif(e.message:findi("challenge eejag")) then
-		eq.spawn2(27119,0,0,262,1124,-2160,0); -- NPC: Eejag
+		eq.spawn2(27119,0,0,274,1141,-32,10); -- NPC: Eejag
 		eq.depop_with_timer();
 	end
 end
