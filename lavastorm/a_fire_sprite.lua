272e67b2827da3d24c19b9bad7ec41c4732e2068
RetributionEQ <info@retributioneq.com>
2024-07-17 10:12:37 -0400

    Pop eejag where sprite is, because I'm tired of invisible walls.

--- lavastorm/a_fire_sprite.lua
+++ lavastorm/a_fire_sprite.lua
@@ -2,7 +2,8 @@ function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Emote("ignores you.");
 	elseif(e.message:findi("challenge eejag")) then
-		eq.spawn2(27119,0,0,274,1141,-32,10); -- NPC: Eejag
+		e.self:Emote("lets loose a loud roar.");
 		eq.depop_with_timer();
+		eq.spawn2(27119,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: Eejag
 	end
 end
