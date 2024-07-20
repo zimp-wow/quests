4b20dd1a252cd89833807fafc471131812ffbfe0
catapultam-habeo <alex@catapult.am>
2024-07-20 20:21:28 +0000

    PD adds add everyone attacking PD to hatelist

---
+++ veeshan/108518.pl
@@ -0,0 +1,16 @@
+sub EVENT_TICK {
+    my $phara_dar = $entity_list->GetNPCByNPCTypeID(108048);
+    my @pd_hatelist = $phara_dar->GetHateList();
+    if (@pd_hatelist) {
+        my $rand_hater = $pd_hatelist[$int(rand(@pd_hatelist))];
+        if ($rand_hater) {
+            $npc->AddToHateList($rand_hater, 1000);
+        }
+
+        foreach my $hle (@pd_hatelist) { # For each value in the array, do something
+            if ($hle) {
+                $npc->AddToHateList($hle->GetEnt(), 1);
+            }
+        }
+    }
+}
