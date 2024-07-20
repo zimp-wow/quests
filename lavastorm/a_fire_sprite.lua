f30e7f2131c61ddb5f52507d80c85950c467562b
RetributionEQ <info@retributioneq.com>
2024-07-20 11:16:58 -0400

    Fix Eejag take 3

--- lavastorm/a_fire_sprite.lua
+++ lavastorm/a_fire_sprite.lua
@@ -4,6 +4,6 @@ function event_say(e)
 	elseif(e.message:findi("challenge eejag")) then
 		e.self:Emote("lets loose a loud roar.");
 		eq.depop_with_timer();
-		eq.spawn2(27119,0,0,e.self:GetX(),e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: Eejag
+		eq.spawn2(27119,0,0,-100, -7, -11, 462); -- NPC: Eejag
 	end
 end
