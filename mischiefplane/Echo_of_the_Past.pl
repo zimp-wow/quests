c0dffe4969b6a12011c7540d8ecfd24af3be5eff
RetributionEQ <info@retributioneq.com>
2024-08-01 15:59:46 -0400

    WHAT, YOU DIDN'T WANT TO GO TO HATE?

--- mischiefplane/Echo_of_the_Past.pl
+++ mischiefplane/Echo_of_the_Past.pl
@@ -1,7 +1,7 @@
-my $expedition_name = "The Plane of Hate";
+my $expedition_name = "The Plane of Mischief";
 my $min_players     = 1;
 my $max_players     = 72;
-my $dz_zone         = "hateplaneb";
+my $dz_zone         = "mischiefplane";
 
 sub EVENT_SAY {
   plugin::OfferStandardInstance($expedition_name, $min_players, $max_players, $dz_zone);
