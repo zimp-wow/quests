b737ac976dcb9cf33cd9bbc55ad7a89b2f752aaa
RetributionEQ <info@retributioneq.com>
2024-08-01 15:12:59 -0400

    Add Echo to PoM

---
+++ mischiefplane/Echo_of_the_Past.pl
@@ -0,0 +1,8 @@
+my $expedition_name = "The Plane of Hate";
+my $min_players     = 1;
+my $max_players     = 72;
+my $dz_zone         = "hateplaneb";
+
+sub EVENT_SAY {
+  plugin::OfferStandardInstance($expedition_name, $min_players, $max_players, $dz_zone);
+}
