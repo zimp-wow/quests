00064e018b681c5a03021c41ca5d02dc263bf86e
RetributionEQ <info@retributioneq.com>
2024-08-06 20:07:58 -0400

    No e.self - Thank you Trust

--- greatdivide/Sentry_Badain.lua
+++ greatdivide/Sentry_Badain.lua
@@ -13,7 +13,7 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 
-	if not war_started and item_lib.check_turn_in(e.self, e.trade, {item1 = 30369, item2= 1567}) then -- 9th ring and declaration of war
+	if not war_started and item_lib.check_turn_in(e.trade, {item1 = 30369, item2= 1567}) then -- 9th ring and declaration of war
 		e.other:AddEXP(20000);
 		e.other:SummonItem(30369);
 		e.self:Say(string.format("I'll be right with you, %s.",e.other:GetCleanName()));
