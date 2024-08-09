a8ebd12f5f41b3670ed54be91f8315b1b3615314
catapultam-habeo <alex@catapult.am>
2024-08-09 07:47:38 +0000

    wtf

--- global/spells/17785.pl
+++ global/spells/17785.pl
@@ -1,10 +1,5 @@
 sub EVENT_SPELL_EFFECT_CLIENT {
     quest::debug("spell_id " . $spell_id);
-    quest::debug("caster_id " . $caster_id);
-    quest::debug("tics_remaining " . $tics_remaining);
-    quest::debug("caster_level " . $caster_level);
-    quest::debug("buff_slot " . $buff_slot);
-    quest::debug("spell " . $spell);
 
     $client->ConsumeItemOnCursor();
 }
