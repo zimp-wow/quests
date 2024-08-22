53b47da4f7ae5ebb65598a3d7365ad956e89c69a
catapultam-habeo <alex@catapult.am>
2024-08-22 01:16:16 +0000

    revert some stuff

--- soldungb/player.pl
+++ soldungb/player.pl
@@ -10,26 +10,11 @@ sub EVENT_TARGET_CHANGE {
 	}
 }
 sub EVENT_ENTERZONE {
-	
-	# this is to detect if you are in the fabled zone when you shouldn't be. 
-	# we have to get the expedition version number if your on one
-	# and the zone version number. If the instanceid = 0, we are in the base zone
-	# if the exp version is >0, we are in the fabled instance instead of the base one. 
-	
-	my $expedition = $client->GetExpedition();
-	my $expversion = $expedition->GetZoneVersion();
-	my $instanceid = $client->GetInstanceID();
-	
-	#$client->Message(7, "Test! 1234 instanceid .. $instanceid");
-	
-	#our instance is the base version of the zone, we don't care.
-	if($instanceid != 1){
-		#$client->Message(7, "Kicking out as instance is less than 1");
+	if (!$client->GetExpedition()) {
 		return;
 	}
-	#our expidition is not version 1 (fabled) so we don't care. 
-	if($expversion != 1) {
-		#$client->Message(7, "Kicking out as exp version is less than 1");
+
+	if ($expedition->GetZoneVersion() != 1) {
 		return;
 	}
 
@@ -38,11 +23,9 @@ sub EVENT_ENTERZONE {
 	$bindx = $client->GetBindX;
 	$bindy = $client->GetBindY;
 	$bindz = $client->GetBindZ;
-
 	
 	if (!plugin::is_stage_complete($client, 'FNagafen')) {
 		$client->Message(7, "You don't belong here!");
 		$client->MovePC($bind, $bindx, $bindy, $bindz, $bindh);
-	 }
-
+	}
 }
