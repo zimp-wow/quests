485b1c5e1d5fc64998adfee2cd447aa9d1fabfae
catapultam-habeo <alex@catapult.am>
2024-09-02 16:02:18 +0000

    rework tearel

--- bazaar/player.pl
+++ bazaar/player.pl
@@ -36,88 +36,107 @@ sub EVENT_POPUPRESPONSE {
         my $group_flg           = quest::get_data($client->AccountID() ."-group-ports-enabled") || "";
         my $attuned_shortname   = $client->GetEntityVariable("magic_map_attune");
         my $waypoint_data       = plugin::GetWaypoint($attuned_shortname, $client);
+        my $group               = $client->GetGroup();
+        my $dz                  = $client->GetExpedition();
 
-        # Check if we have valid config data, either a waypoint or an expedition
-        if ($waypoint_data || ($attuned_shortname eq 'instance' && $client->GetExpedition())) {
-            my $long_name = $waypoint_data->[0];  # Access the long name (first element in the array)
-            my $continent = $waypoint_data->[1];  # Access the continent ID
-            my $x = $waypoint_data->[2];          # X coordinate
-            my $y = $waypoint_data->[3];          # Y coordinate
-            my $z = $waypoint_data->[4];          # Z coordinate
-            my $heading = $waypoint_data->[5];    # Heading
+        # Group option locked behind EoM
+        if ($popupid == 1461 && !$group_flg) {
+            plugin::YellowText("Your account does not have the ability to transport your group this way unlocked.");
+            return;
+        }
 
-            # Group option locked behind EoM
-            if ($popupid == 1461 && !$group_flg) {
+        if ($attuned_shortname eq 'instance') {
+            if (!$group_flg) {
                 plugin::YellowText("Your account does not have the ability to transport your group this way unlocked.");
                 return;
             }
 
-            # Group port option
-            if ($popupid == 1461) { 
-                my $group = $client->GetGroup();
-                my $dz    = $client->GetExpedition();
-                if ($group) {
-                    
-                    # DZ validation branch
-                    if ($dz) {
-                        # Get the expedition object
-                        my $dz = $client->GetExpedition();
-                        if ($dz) {
-                            my $expedition_members_ref = $dz->GetMembers();
-                            my %expedition_members = %{$expedition_members_ref};
+            if (!$dz) {
+                plugin::YellowText("You are not a member of an expedition.");
+                return;
+            }
 
-                            for (my $count = 0; $count < $group->GroupCount(); $count++) {
-                                my $player = $group->GetMember($count);
-                                if ($player) {
-                                    # Check if the player is a member of the expedition
-                                    my $player_name = $player->GetName();
-                                    if ($dz && !exists $expedition_members{$player_name}) {
-                                        plugin::YellowText("$player_name is not a member of this expedition.");
-                                        return;
-                                    }
-                                }
-                            }
+            if (!plugin::is_eligible_for_zone($client, quest::GetZoneShortName($dz->GetZoneID()))) {
+                plugin::YellowText("You are not eligible to enter that zone.");
+                return;
+            }
+            
+            if ($popupid == 1461 && $group) {
+                my $expedition_members_ref = $dz->GetMembers();
+                my %expedition_members = %{$expedition_members_ref};
+                for (my $count = 0; $count < $group->GroupCount(); $count++) {
+                    my $player = $group->GetMember($count);
+                    if ($player) {
+                        # Check if the player is a member of the expedition
+                        # TODO - Collect all failure modes and output at once instead of breaking on first one
+                        my $player_name = $player->GetName();
+                        if (!exists $expedition_members{$player_name}) {
+                            plugin::YellowText("$player_name is not a member of this expedition.");
+                            return;
                         }
-                    }
 
-                    # If all checks pass, move the group members who are within range
-                    for (my $count = 0; $count < $group->GroupCount(); $count++) {
-                        my $player = $group->GetMember($count);
-                        if ($player && plugin::is_eligible_for_zone($player->CastToClient(), $attuned_shortname, 1)) {
-                            if ($player == $client) {
-                                next;
-                            }
-
-                            #DZ Branch
-                            if ($dz && $attuned_shortname eq 'instance') {
-                                $player->SpellEffect(218,1);
-                                $player->MovePCDynamicZone($dz->GetZoneID());
-                                next;
-                            }
-
-                            #Attuned Branch
-                            if ($attuned_shortname ne 'instance') {
-                                $player->SpellEffect(218,1);
-                                $player->MovePC(quest::GetZoneID($attuned_shortname), $x, $y, $z, $heading);
-                                next;
-                            }
-
-                            quest::debug("Failed to find valid port condition for " . $player->GetCleanName());
+                        if (!plugin::is_eligible_for_zone($player, quest::GetZoneShortName($dz->GetZoneID()))) {
+                            plugin::YellowText("$player_name is not eligible to enter that zone.");
                             return;
                         }
                     }
                 }
+
+                for (my $count = 0; $count < $group->GroupCount(); $count++) {
+                    my $player = $group->GetMember($count);
+                    if ($player) {
+                        $player->SpellEffect(218,1);
+                        $player->MovePCDynamicZone($dz->GetZoneID());
+                    }
+                }
             }
 
-            if ($attuned_shortname eq 'instance' && $client->GetExpedition()) {
-                $dz = $client->GetExpedition();
-                $client->SpellEffect(218,1);
-                $client->MovePCDynamicZone($dz->GetZoneID());
-            } else {                
-                $client->SpellEffect(218,1);
-                $client->MovePC(quest::GetZoneID($attuned_shortname), $x, $y, $z, $heading);
-            }            
-        }        
+            $client->SpellEffect(218,1);
+            $client->MovePCDynamicZone($dz->GetZoneID());
+        } else {
+            # Not moving to a DZ
+            if (!$waypoint_data) {
+                plugin::YellowText("You have selected an invalid destination.");
+                return;
+            }
+
+            my $long_name = $waypoint_data->[0];  # Access the long name (first element in the array)
+            my $continent = $waypoint_data->[1];  # Access the continent ID
+            my $x         = $waypoint_data->[2];  # X coordinate
+            my $y         = $waypoint_data->[3];  # Y coordinate
+            my $z         = $waypoint_data->[4];  # Z coordinate
+            my $heading   = $waypoint_data->[5];  # Heading
+
+            if (!plugin::is_eligible_for_zone($client, $attuned_shortname)) {
+                plugin::YellowText("You are not eligible to enter that zone.");
+                return;
+            }
+
+            if ($popupid == 1461 && $group) {
+                for (my $count = 0; $count < $group->GroupCount(); $count++) {
+                    my $player = $group->GetMember($count);
+                    if ($player) {
+                        # TODO - Collect all failure modes and output at once instead of breaking on first one
+                        my $player_name = $player->GetName();
+                        if (!plugin::is_eligible_for_zone($player, $attuned_shortname)) {
+                            plugin::YellowText("$player_name is not eligible to enter that zone.");
+                            return;
+                        }
+                    }
+                }
+
+                for (my $count = 0; $count < $group->GroupCount(); $count++) {
+                    my $player = $group->GetMember($count);
+                    if ($player) {
+                        $player->SpellEffect(218,1);
+                        $player->MovePC(quest::GetZoneID($attuned_shortname), $x, $y, $z, $heading);
+                    }
+                }
+            }
+
+            $client->SpellEffect(218,1);
+            $client->MovePC(quest::GetZoneID($attuned_shortname), $x, $y, $z, $heading);
+        }
     }
 }
 
