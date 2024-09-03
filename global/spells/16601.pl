f484c616c01e89a66d245309b3cac33e61fb5cc9
catapultam-habeo <alex@catapult.am>
2024-09-02 21:24:48 -0500

    Revert "resupply agents and bankers"
    This reverts commit 6c6efc1baad9be6190c4323089f2c73d10551a81.

--- global/spells/16601.pl
+++
@@ -1,9 +0,0 @@
-# Summon Clockwork Banker
-sub EVENT_SPELL_EFFECT_CLIENT {
-    my $npc_id = quest::spawn2(772, 0, 0, $client->GetX(), $client->GetY(), $client->GetZ(), int(rand(512)) + 1);
-    my $npc = $entity_list->GetMobByID($npc_id);
-    $npc->TempName($client->GetCleanName() . '`s_Banker_Buddy');
-
-    #Testing Value
-    $npc->SetTimer("despawn", 840);
-}
--- global/spells/16995.pl
+++
@@ -1,9 +0,0 @@
-# Summon (Clockwork) Resupply Agent
-sub EVENT_SPELL_EFFECT_CLIENT {
-    my $npc_id = quest::spawn2(771, 0, 0, $client->GetX(), $client->GetY(), $client->GetZ(), int(rand(512)) + 1);
-    my $npc = $entity_list->GetMobByID($npc_id);
-    $npc->TempName($client->GetCleanName() . '`s_Resupply_Agent');
-
-    #Testing Value
-    $npc->SetTimer("despawn", 840);
-}
