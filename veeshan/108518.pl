63f04815bfb07c1a5e89f6778b539aa3a008b9c5
catapultam-habeo <alex@catapult.am>
2024-09-23 14:45:45 +0000

    fix pd event script

--- veeshan/108518.pl
+++ veeshan/108518.pl
@@ -1,15 +1,17 @@
 sub EVENT_TICK {
     my $phara_dar = $entity_list->GetNPCByNPCTypeID(108048);
-    my @pd_hatelist = $phara_dar->GetHateList();
-    if ($phara_dar && @pd_hatelist) {
-        my $rand_hater = $pd_hatelist[$int(rand(@pd_hatelist))];
-        if ($rand_hater) {
-            $npc->AddToHateList($rand_hater, 1000);
-        }
+    if ($phara_dar) {
+        my @pd_hatelist = $phara_dar->GetHateList();
+        if (@pd_hatelist) {
+            my $rand_hater = $pd_hatelist[int(rand(@pd_hatelist))];  # Ensure rand produces an integer
+            if ($rand_hater) {
+                $npc->AddToHateList($rand_hater->GetEnt(), 1000);  # Add the random hater to the hate list
+            }
 
-        foreach my $hle (@pd_hatelist) { # For each value in the array, do something
-            if ($hle) {
-                $npc->AddToHateList($hle->GetEnt(), 1);
+            foreach my $hle (@pd_hatelist) {  # For each value in the array, do something
+                if ($hle) {
+                    $npc->AddToHateList($hle->GetEnt(), 1);  # Add each hater to the hate list
+                }
             }
         }
     }
