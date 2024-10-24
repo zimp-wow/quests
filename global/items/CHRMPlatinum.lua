815907b70f6cdc20681b0c923710b98d60a1a25a
catapultam-habeo <alex@catapult.am>
2024-10-24 20:32:32 +0000

    fix platinum disc

--- global/items/CHRMPlatinum.lua
+++ global/items/CHRMPlatinum.lua
@@ -3,8 +3,14 @@
 function event_scale_calc(e)
     local money = e.owner:GetAllMoney() / 100000000;
 
-    if(money < 0) then
+    -- Ensure money is at least 0
+    if money < 0 then
         money = 0;
+    end
+
+    -- Apply a log scale for money greater than 1 (100k platinum)
+    if money > 1 then
+        money = 1 + math.log10(money);
     end
 
     e.self:SetScale(money);
