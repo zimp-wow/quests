ce89c5ce83a14a9ad347ae303012473d2b0054d8
catapultam-habeo <alex@catapult.am>
2024-11-03 20:48:30 +0000

    re-introduce bazaar idle script for bazaar. use single static instead of new instance for each player

--- bazaar/player.pl
+++ bazaar/player.pl
@@ -141,65 +141,81 @@ sub EVENT_POPUPRESPONSE {
 }
 
 sub EVENT_ENTERZONE {
-    set_current_position();
-    quest::settimer("check_idle", get_idle_time());
+    if (plugin::IsTHJ()) {
+        set_current_position();
+        quest::settimer("check_idle", get_idle_time());
+    }
 }
 
 sub EVENT_CONNECT {
-    set_current_position();
-    quest::settimer("check_idle", get_idle_time());
+    if (plugin::IsTHJ()) {
+        set_current_position();
+        quest::settimer("check_idle", get_idle_time());
+    }
 }
 
 sub EVENT_TIMER {
-    return; # NOP THIS
-    quest::settimer("check_idle", get_idle_time());
-    if ($timer eq "check_idle") {
+    # Constants for idle times and intervals
+    use constant {
+        IDLE_THRESHOLD_SECONDS   => 300,   
+        WARNING_START_MINUTES    => 4,
+        WARNING_INTERVAL_SECONDS => 60,
+    };
+
+    my $shadow_bazaar = quest::get_data("shadow_bazaar_instanceid");
+
+    if (!$shadow_bazaar) {
+        quest::debug("Creating Shadow Bazaar");
+        $shadow_bazaar = quest::CreateInstance('bazaar', 0, 604800);
+        quest::set_data("shadow_bazaar_instanceid", $shadow_bazaar, 604800);
+    }
+
+    $client->AssignToInstance($shadow_bazaar);
+
+    #quest::settimer("check_idle", get_idle_time());
+    
+    if ($timer eq "check_idle") { 
         if ($client->IsTrader() || $client->GetGM()) {
-            # NOP
+            return;
         }
-        else {
-            my $idle        = check_position();
-            my $idle_track  = $client->GetEntityVariable("idle_warning") ? int($client->GetEntityVariable("idle_warning")) : 0;
-            my $non_idle_c  = $client->GetEntityVariable("non_idle_count") ? int($client->GetEntityVariable("non_idle_count")) : 1;
+   
+        my $idle        = check_position();
+        my $idle_track  = $client->GetEntityVariable("idle_warning") ? int($client->GetEntityVariable("idle_warning")) : 0;
+        my $non_idle_c  = $client->GetEntityVariable("non_idle_count") ? int($client->GetEntityVariable("non_idle_count")) : 1;
 
-            $client->SetEntityVariable("idle_warning", $idle_track + 1);
-            if ($instanceid) { # inside private instance   
-                if ($idle and $idle_track % 300 == 0) {
-                    $client->Message(15, "You are in a private Bazaar instance. You will be transported to the public Bazaar once you are no longer idle.");
-                }
-                elsif (!$idle) {
-                    if ($non_idle_c < 3) {
-                        $client->SetEntityVariable("non_idle_count", $non_idle_c + 1);
-                    } else {
-                        $client->Message(15, "You are no longer idle. Returning you to the public Bazaar.");
-                        $client->RemoveFromInstance($instanceid);
-
-                        quest::DestroyInstance($instanceid);
-
-                        $client->MovePC(151, $client->GetX(), $client->GetY(), $client->GetZ(), $client->GetHeading());
-                    }                    
-                }
+        $client->SetEntityVariable("idle_warning", $idle_track + get_idle_time());
+        quest::debug("check_idle tick - instanceid:$instanceid - idle_track: $idle_track");
+        
+        if ($instanceid == $shadow_bazaar) { # inside private instance   
+            if ($idle and $idle_track % (IDLE_THRESHOLD_SECONDS) == 0) {
+                $client->Message(15, "You are in the Shadow Bazaar. You will be transported to the Bazaar once you are no longer idle.");
             }
-            else { # inside public bazaar
-                if (!$idle ) {
-                    $client->DeleteEntityVariable("idle_warning");
-                }
-                elsif ($idle and $idle_track >= (300 * 2)) {
-                    # my $instance = quest::CreateInstance('bazaar', 0, 144000000);
-                    # $client->Message(15, "You have been idle for ten minutes. Transporting you to a private instance of the Bazaar...");                    
-                    # $client->AssignToInstance($instance);
-                    # $client->MovePCInstance(151, $instance, $client->GetX(), $client->GetY(), $client->GetZ(), $client->GetHeading());
-                }
-                elsif ($idle) {
-                    my $minutes_idle = int($idle_track / 60);
-                    if ($idle_track % 60 == 0 and $minutes_idle >= 5) { # Trigger a warning every minute
-                        my %number_words = (1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine");
-                        my $minutes_idle_word = $number_words{$minutes_idle} // $minutes_idle;
-                        $client->Message(15, "You have been idle for $minutes_idle_word minutes. You will be transported to a private instance of the Bazaar soon.");
-                    }
-                }
+            elsif (!$idle) {
+                #if ($non_idle_c < 3) {
+                #    $client->SetEntityVariable("non_idle_count", $non_idle_c + 1);
+                #} else {
+                    $client->Message(15, "You are no longer idle. Returning you to the Bazaar.");
+                    $client->MovePC(151, $client->GetX(), $client->GetY(), $client->GetZ(), $client->GetHeading());
+                #}                    
             }
         }
+        else { # inside public bazaar
+            if (!$idle) {
+                $client->DeleteEntityVariable("idle_warning");
+            }
+            elsif ($idle and $idle_track >= (IDLE_THRESHOLD_SECONDS)) {
+                $client->Message(15, "You have been idle for " . (IDLE_THRESHOLD_SECONDS / 60) . " minutes. Transporting you to the Shadow Bazaar...");
+                $client->MovePCInstance(151, $shadow_bazaar, $client->GetX(), $client->GetY(), $client->GetZ(), $client->GetHeading());
+            }
+            elsif ($idle) {
+                my $minutes_idle = int($idle_track / 60);
+                if ($idle_track % WARNING_INTERVAL_SECONDS == 0 and $minutes_idle >= WARNING_START_MINUTES) {
+                    my %number_words = (1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine");
+                    my $minutes_idle_word = $number_words{$minutes_idle} // $minutes_idle;
+                    $client->Message(15, "You have been idle for $minutes_idle_word minutes. You will be transported to the Shadow Bazaar soon.");
+                }
+            }
+        }        
     }
 }
 
@@ -225,5 +241,5 @@ sub set_current_position {
 }
 
 sub get_idle_time {
-    return 1;
+    return 10;
 }
