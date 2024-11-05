23141a2f0b29e026798505f5eaf6de49f0fa599e
catapultam-habeo <alex@catapult.am>
2024-11-05 22:16:06 +0000

    duplicate subrouteine def

--- plugins/cata_general.pl
+++ plugins/cata_general.pl
@@ -1,27 +1,3 @@
-#Used for Bic
-sub ClassType {
-    my $client = plugin::val('$client');
-    my $class  = $client->GetClass();
-
-    my %melee_classes = map { $_ => 1 } (1, 7, 9, 16);
-    my %caster_classes = map { $_ => 1 } (11, 12, 13, 14);
-    my %priest_classes = map { $_ => 1 } (2, 6, 10);
-
-    if (exists $melee_classes{$class}) {
-        return "melee";
-    }
-
-    if (exists $caster_classes{$class}) {
-        return "caster";
-    }
-
-    if (exists $priest_classes{$class}) {
-        return "priest";
-    }
-
-    return "hybrid";
-}
-
 #Usage: plugin::CheckCashPayment(target_val_in_copper, $copper, $silver, $gold, $platinum)
 # Returns false if insufficient payment
 # Returns true if sufficient payment
