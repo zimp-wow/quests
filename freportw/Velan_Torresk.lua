ad52b6c8294fae3c46d0c71da7987e63e8c07ec4
RetributionEQ <info@retributioneq.com>
2024-11-06 17:06:56 -0500

    Fix LUA not perl dummy

--- freportw/Velan_Torresk.lua
+++ freportw/Velan_Torresk.lua
@@ -7,13 +7,13 @@ function event_say(e)
 	elseif(e.message:findi("orcs")) then
 		e.self:Say("The Deathfist Orcs are a large tribe of Orcs who live out in the desert. They are constantly sending small raiding parties and scouts to attack Freeport and its citizens.");
 	elseif(e.message:findi("white training sash")) then
-		if(fac < 4) then
+		if(fac < 5) then
 			e.self:Say("The white training sash of the Ashen Order is awarded to our new members for completing a few simple tasks to prove their devotion to our clan. As you know, Freeport is a very hostile place, under constant attack from [orcs], wild beasts, and even the undead. To help keep this city and its citizens relatively safe, we must help the Militia protect the main gates. Bring me two Deathfist pawn scalps from those vile [Deathfist orcs], a snake fang, and some bone chips from a skeleton. Good luck, " .. e.other:GetName() .. ", represent us well!");
 		else
 			e.self:Say("I have been watching you, and appreciate the help you've given to the brothers and sisters of the Ashen Order, but I feel that such a vital matter should be left to one of our more trusted members.");
 		end
 	elseif(e.message:findi("yellow sash of order")) then
-		if(fac < 4) then
+		if(fac < 5) then
 			e.self:Say("To earn the yellow sash, you must prove yourself to be very skilled in the art of fighting. The lands to the west and south of Freeport are filled with dangerous beasts that often prey upon innocent travelers. Help protect our merchant caravans and traveling citizens, while at the same time practicing your defensive skills, and eliminating these deadly creatures from the surrounding landscape. Bring me a giant snake rattle, a Deathfist slashed belt, the chitin of a desert tarantula, and turn in your white training sash, and I shall reward your noble work with our yellow Sash of Order. Good luck, " .. e.other:GetName() .. "!");
 		else
 			e.self:Say("I have been watching you, and appreciate the help you've given to the brothers and sisters of the Ashen Order, but I feel that such a vital matter should be left to one of our more trusted members.");
@@ -28,7 +28,7 @@ function event_trade(e)
 	local fac = e.other:GetFaction(e.self);
 
 	-- Kindly Faction - Deathfist Pawn Scalp - Snake Fang - Bone Chips
-	if(fac < 4) then
+	if(fac < 5) then
 		if(item_lib.check_turn_in(e.trade, {item1 = 13794,item2 = 13794,item3 = 13067,item4 = 13073})) then
 			e.self:Say("Good work, " .. e.other:GetName() .. ", you've worked hard and proven yourself a valuable addition to the Ashen Order. Here's your white sash, wear it with pride.");
 			e.other:SummonItem(10130); -- White Training Sash
