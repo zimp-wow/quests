dc247143f225253f284448946ff581da9644243b
MazeEQ <36613206+MazeEQ@users.noreply.github.com>
2024-11-09 13:13:45 -0500

    Convert to DataBucket

--- airplane/Bazzt_Zzzt.lua
+++ airplane/Bazzt_Zzzt.lua
@@ -1,4 +1,7 @@
+function event_spawn(e)
+	instance_id = eq.get_zone_instance_id() or 0;
+end
 function event_death_complete(e)
-	eq.set_global("sirran","6",3,"M20");
-	eq.spawn2(71058,0,0,234,-1078,812,384); --Sirran the Lunatic
+	eq.set_data("airplane-sirran-".. instance_id, "7");
+	eq.spawn2(71058,0,0,-960,-1037,1093,128); -- NPC: Sirran_the_Lunatic
 end
