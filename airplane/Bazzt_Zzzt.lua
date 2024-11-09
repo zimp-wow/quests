8c043f6963a76286786226a52f0d70f50c41e3dc
MazeEQ <36613206+MazeEQ@users.noreply.github.com>
2024-11-09 18:46:46 -0500

    Fix Isle 6 Sirran

--- airplane/Bazzt_Zzzt.lua
+++ airplane/Bazzt_Zzzt.lua
@@ -3,5 +3,5 @@ function event_spawn(e)
 end
 function event_death_complete(e)
 	eq.set_data("airplane-sirran-" .. instance_id, "7", tostring(eq.seconds("24h")));
-	eq.spawn2(71058,0,0,-960,-1037,1093,128); -- NPC: Sirran_the_Lunatic
+	eq.spawn2(71058,0,0,234,-1078,812,384); --Sirran the Lunatic
 end
