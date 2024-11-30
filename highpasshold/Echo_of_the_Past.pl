60cd61a84db5c688e78b76ab11e41ae2576409da
catapultam-habeo <alex@catapult.am>
2024-11-30 01:34:00 +0000

    fix hph dz

--- highpasshold/Echo_of_the_Past.pl
+++ highpasshold/Echo_of_the_Past.pl
@@ -1,7 +1,7 @@
 my $expedition_name = "Highpass Hold";
 my $min_players     = 1;
 my $max_players     = 72;
-my $dz_zone         = "highpass";
+my $dz_zone         = "highpasshold";
 
 sub EVENT_SAY {
   plugin::OfferStandardInstance($expedition_name, $min_players, $max_players, $dz_zone);
