a84a2ce3f03e5955581c02eab12485dafdd23de2
catapultam-habeo <alex@catapult.am>
2024-12-14 06:21:14 +0000

    more petamorph wands

--- bazaar/A_Mischievous_Halfling.pl
+++ bazaar/A_Mischievous_Halfling.pl
@@ -38,7 +38,7 @@ sub GetRandomResult {
         # { chance => 25, items => [36013], message => "Cheers!" }, #Thanksgiving
         { chance => 25, items => [56942, 56940, 56938, 56934, 56935, 56944, 56937, 32557], message => "Better luck next time! Here, drink this down, it should cheer you up!" },
         { chance => 20, items => [41961, 48083], message => "Let the experience flow!" },
-        { chance => 10, items => [67923, 56052, 61036, 62774, 66783, 64711, 66431, 66449, 67953, 67883, 52193, 66598, 61979, 67145, 66564], message => "Nicely done! Pets love a facelift every now and then $name!" },
+        { chance => 10, items => [67923, 56052, 61036, 62774, 66783, 64711, 66431, 66449, 67953, 67883, 52193, 66598, 61979, 67145, 66564, 907290, 907291, 907292], message => "Nicely done! Pets love a facelift every now and then $name!" },
         { chance => 5, items => [37954, 67008, 43993, 37999, 40638, 40714, 40686, 50854, 40656, 31861], message => "An illusion?! In case I don't recognize you next time, congratulations $name!" },
         { chance => 3, items => [59508, 59513, 43970, 57798, 54983, 60437, 52098, 64560, 54934, 60945, 62787, 57191], message => "Wow! Looks like $name will be traveling in style now!" },
         { chance => 2, items => [52024], message => "Urthron's... what?! How'd that get in there?! Congratulations $name!" },
