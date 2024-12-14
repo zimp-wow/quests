79be73b2de2105771b62bb6ad34717c204f07c2f
catapultam-habeo <alex@catapult.am>
2024-12-14 03:12:45 +0000

    lol it wil lbe crazy if this works

--- global/items/CHRMNoBuffs.lua
+++ global/items/CHRMNoBuffs.lua
@@ -1,9 +1,30 @@
 -- CHRMNoBuffs
 
+function GetBeneBuffCount(client)
+    if client == nil then
+        return 0 -- No buffs if client is nil
+    end
+
+    local count = 0
+    local maxBuffSlots = client:GetMaxBuffSlots()
+
+    for i = 0, maxBuffSlots - 1 do
+        local spell_id = client:GetBuffSpellID(i)
+        if spell_id ~= -1 then -- Check if a valid spell exists in this buff slot
+            local spell = Spell(spell_id)
+            if spell and spell:IsBeneficial() then
+                count = count + 1
+            end
+        end
+    end
+
+    return count
+end
+
 function event_scale_calc(e)
-    if(e.owner:GetBeneBuffCount() == 0) then
-        e.self:SetScale(1);
+    if GetBeneBuffCount(e.owner) == 0 then
+        e.self:SetScale(1) -- Scale to 1 if no beneficial buffs
     else
-        e.self:SetScale(0);
+        e.self:SetScale(0) -- Scale to 0 if there are beneficial buffs
     end
 end
