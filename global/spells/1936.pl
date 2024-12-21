4d892add20486204d36a12d22bd816a33b564b8b
Catapultam Habeo <alex@catapult.am>
2024-12-20 23:03:36 -0600

    wtf kk

--- global/spells/1936.pl
+++ global/spells/1936.pl
@@ -1,5 +1,5 @@
 sub EVENT_SPELL_EFFECT_CLIENT {
-    my $slot = $client->FindSpellBookSlotBySpellID(1936);
+    my $slot = $client->GetSpellBookSlotBySpellID(1936);
     if ($slot < 0) {
         $client->ScribeSpell(1936, $client->GetFreeSpellBookSlot(), 1);
     }
