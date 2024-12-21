584ab553fd62ea2d5eb2a0c10f7ac113ce604110
Catapultam Habeo <alex@catapult.am>
2024-12-20 23:02:29 -0600

    fix

--- global/spells/1936.pl
+++ global/spells/1936.pl
@@ -1,5 +1,5 @@
 sub EVENT_SPELL_EFFECT_CLIENT {
-    my $slot = $client->FindMemmedSpellBySpellID(1936);
+    my $slot = $client->FindSpellBookSlotBySpellID(1936);
     if ($slot < 0) {
         $client->ScribeSpell(1936, $client->GetFreeSpellBookSlot(), 1);
     }
