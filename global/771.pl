d494e3778232fbe3448bbb7dd8b1d1d85e5b37f5
Catapultam Habeo <alex@catapult.am>
2024-12-22 12:02:41 -0600

    don't depop clockworks until owner leaves

--- global/771.pl
+++ global/771.pl
@@ -1,15 +1,4 @@
-# Clockwork Banker
-
-sub EVENT_SPAWN {
-    $npc->SetTimer("depop", 15 * 60);
-}
-
-sub EVENT_TIMER {
-    if ($timer eq "depop") {
-        quest::debug("depop");
-        $npc->Depop();
-    }
-}
+# Clockwork Resupply Agent
 
 sub EVENT_TICK {
     if (!$npc->GetEntityVariable("owner") || !$entity_list->GetClientByCharID($npc->GetEntityVariable("owner"))) {
--- global/772.pl
+++ global/772.pl
@@ -1,16 +1,5 @@
 # Clockwork Banker
 
-sub EVENT_SPAWN {
-    $npc->SetTimer("depop", 15 * 60);
-}
-
-sub EVENT_TIMER {
-    if ($timer eq "depop") {
-        quest::debug("depop");
-        $npc->Depop();
-    }
-}
-
 sub EVENT_TICK {
     if (!$npc->GetEntityVariable("owner") || !$entity_list->GetClientByCharID($npc->GetEntityVariable("owner"))) {
         $npc->Depop();
