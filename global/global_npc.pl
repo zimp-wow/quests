e1f43a00adc55a21f281eba20a36eeeaba9b4f96
Catapultam Habeo <alex@catapult.am>
2024-12-22 11:19:46 -0600

    improve performance on mob death script

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -185,7 +185,7 @@ sub EVENT_KILLED_MERIT {
                 plugin::LootEOM($client, $eom_loot_amount);
                 $client->SendSound();
 
-                quest::set_data($client->AccountID() . "-eom-event-scale", quest::get_data($client->AccountID() . "-eom-event-scale") + 1);
+                $client->SetBucket("eom-event-scale", $client->GetBucket("eom-event-scale") + 1);
             } else {
                 if ($client->GetGM()) {
                     quest::debug("Failed to roll out of $eom_drop_chance");
