b2461afd201abe501c4e47de69d2fff68f3b1df6
Fryguy <fryguy503@gmail.com>
2025-01-03 13:32:27 -0500

    [Bug Fix] Fix follow code (#87)

--- thurgadina/Avalanche.lua
+++ thurgadina/Avalanche.lua
@@ -3,10 +3,10 @@ function event_spawn(e)
 end
 
 function event_timer(e)
+	eq.stop_timer(e.timer);
 	if e.timer == "follow" then
 		if eq.get_entity_list():IsMobSpawnedByNpcTypeID(115102) then
-			e.self:follow(115102);
-			eq.stop_timer("follow");
+			eq.follow(eq.get_entity_list():GetMobByNpcTypeID(115102):GetID());
 		end
 	end
 end
