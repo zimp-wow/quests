7e540cef6d463d04d8f9e4580c46999a89ed4704
Fryguy <fryguy503@gmail.com>
2025-01-07 12:34:08 -0500

    [Bug] Demi-Lich Order of operations on turn-in (#99)
    * [Bug] Demi-Lich Order of operations on turn-in
    
    * Remove un-needed check

--- overthere/Ixpacan.lua
+++ overthere/Ixpacan.lua
@@ -19,22 +19,24 @@ function event_trade(e)
 	local item_lib = require("items");
 	local qglobals = eq.get_qglobals(e.self,e.other);
 
-	if item_lib.check_turn_in(e.trade, {item1 = 4267}) then -- Item: Necromancer Skullcap
+	if tonumber(qglobals.necskullquest) == 9  then
+		if item_lib.check_turn_in(e.trade, {item1 = 48044, item2 = 4267}) then -- Items: Child of Charasis Remains, Necromancer Skullcap
+			e.self:Say("I see now that I lack the skill necessary to control the Dark Arts. Maybe it would be wiser to allow another such as yourself to continue forward. Please accept this token as a reward in your mastering of the Dark Arts.");
+			eq.delete_global("necskullquest");	-- Removed 8th cap and global, must do quest again from cap 1
+			e.other:SummonItem(48043);			-- Item: Demi Lich Skullcap
+		elseif item_lib.check_turn_in(e.trade, {item1 = 48042}) then -- Item: Ixpacan's Tome (Tome-Full)
+			e.self:Say("Wonderful! You have brought all of the items I have asked for. Your future seems very bright with the rest of the Sages. Step back now as I conjure the child of Charasis.");
+			eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, MT.LightGray, 150, "As Ixpacan starts his incantations, you can see an image begin to appear from the shadows.");
+			e.self:Say("It's out of my control! Defeat it before it destroys us both!");
+			eq.spawn2(93189,0,0,e.self:GetX() + 5,e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: child_of_Charasis
+		elseif item_lib.check_turn_in(e.trade, {item1 = 48044}) then -- Item: Child of Charasis Remains
+			e.self:Say("I don't have the power to process these remains, aid me with your Necromancer Skullcap and provide me the remains and cap!")
+			e.other:SummonItem(48044);	-- Item: Child of Charasis Remains
+		end
+	elseif item_lib.check_turn_in(e.trade, {item1 = 4267}) then -- Item: Necromancer Skullcap
 		eq.set_global("necskullquest", "9", 5,"F"); -- Completed Skullcap Quest 9.1
 		e.self:Say("Oh, I see you are truly gifted in the dark arts. Well I will explain my dilemma to you now if you [wish to hear].");
 		e.other:SummonItem(4267); -- Item: Necromancer Skullcap
-	elseif tonumber(qglobals.necskullquest) == 9 and item_lib.check_turn_in(e.trade, {item1 = 48042}) then -- Item: Ixpacan's Tome (Tome-Full)
-		e.self:Say("Wonderful! You have brought all of the items I have asked for. Your future seems very bright with the rest of the Sages. Step back now as I conjure the child of Charasis.");
-		eq.local_emote({e.self:GetX(), e.self:GetY(), e.self:GetZ()}, MT.LightGray, 150, "As Ixpacan starts his incantations, you can see an image begin to appear from the shadows.");
-		e.self:Say("It's out of my control! Defeat it before it destroys us both!");
-		eq.spawn2(93189,0,0,e.self:GetX() + 5,e.self:GetY(),e.self:GetZ(),e.self:GetHeading()); -- NPC: child_of_Charasis
-	elseif tonumber(qglobals.necskullquest) == 9 and item_lib.check_turn_in(e.trade, {item1 = 48044}) then -- Item: Child of Charasis Remains
-		e.self:Say("I don't have the power to process these remains, aid me with your Necromancer Skullcap and provide me the remains and cap!")
-		e.other:SummonItem(48044);	-- Item: Child of Charasis Remains
-	elseif tonumber(qglobals.necskullquest) == 9 and item_lib.check_turn_in(e.trade, {item1 = 48044, item2 = 4267}) then -- Items: Child of Charasis Remains, Necromancer Skullcap
-		e.self:Say("I see now that I lack the skill necessary to control the Dark Arts. Maybe it would be wiser to allow another such as yourself to continue forward. Please accept this token as a reward in your mastering of the Dark Arts.");
-		eq.delete_global("necskullquest");	-- Removed 8th cap and global, must do quest again from cap 1
-		e.other:SummonItem(48043);			-- Item: Demi Lich Skullcap
 	end
 
 	item_lib.return_items(e.self, e.other, e.trade)
