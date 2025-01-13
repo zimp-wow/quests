d0fa4beacac53e1f3b42c4b1af30bd3470f33502
Fryguy <fryguy503@gmail.com>
2025-01-13 15:26:15 -0500

    [Bug] Correct Emerald Warrior Charm Faction (#109)

--- global/items/CHRMEmeraldWarrior.lua
+++ global/items/CHRMEmeraldWarrior.lua
@@ -1,13 +1,11 @@
 -- CHRMEmeraldWarrior
 
 function event_scale_calc(e)
+    local faction = e.owner:GetModCharacterFactionLevel(326); -- Emerald Warriors
 
-    local faction = e.owner:GetModCharacterFactionLevel(92); -- Emerald Warriors
-
-    if(faction < 0) then
+    if faction < 0 then
         faction = 0;
     end
 
     e.self:SetScale(faction/1500);
-
 end
