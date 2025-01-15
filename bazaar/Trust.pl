4698782c25e03f623b87fd2ead18fffe92947b3e
RetributionEQ <120991589+RetributionEQ@users.noreply.github.com>
2025-01-15 15:07:55 -0500

    Add Trust and Akk

---
+++ bazaar/Trust.pl
@@ -0,0 +1,14 @@
+sub EVENT_SPAWN {
+  quest::settimer("appearance", 1);
+}
+
+sub EVENT_TIMER {
+  quest::stoptimer("appearance");
+  $npc->SetAppearance(1);
+}
+
+sub EVENT_SAY {
+  if ($text=~/hail/i) {
+    quest::say("Welcome, Hero! Have you done a quest recently? I helped make them exactly how you remembered. Thank you for being here!");
+  }  
+}
