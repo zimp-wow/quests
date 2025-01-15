7c32c82e7f73f743f2d0c73bd06320b26f784f41
RetributionEQ <120991589+RetributionEQ@users.noreply.github.com>
2025-01-15 14:23:49 -0500

    Add Neckkola

---
+++ bazaar/Neckkola.pl
@@ -0,0 +1,14 @@
+sub EVENT_SPAWN {
+  quest::settimer("appearance", 1);
+}
+
+sub EVENT_TIMER {
+  quest::stoptimer("appearance");
+  $npc->SetAppearance(1);
+}
+
+sub EVENT_SAY {
+  if ($text=~/hail/i) {
+    quest::say("Welcome, Hero! Are you enjoying the Bazaar? I helped make it possible. Thank you for being here!");
+  }  
+}
